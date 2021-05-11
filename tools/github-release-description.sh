327c8c713a3dd5beee9f8882907b29750035398a
Guillaume Bougard <gbougard@teclib.com>
2021-05-11 09:43:07 +0200

    build: add rpms to release page

--- tools/github-release-description.sh
+++ tools/github-release-description.sh
@@ -11,6 +11,7 @@ fi
 
 if [ "${TAG#*-}" == "$TAG" ]; then
     DEBREV="-1"
+    RPMREV="-1"
 fi
 
 cat >release-description.md <<DESCRIPTION
@@ -50,4 +51,26 @@ ESX | [glpi-agent-task-esx_${TAG}${DEBREV}_all.deb](../../releases/download/$TAG
 Collect | [glpi-agent-task-collect_${TAG}${DEBREV}_all.deb](../../releases/download/$TAG/glpi-agent-task-collect_${TAG}${DEBREV}_all.deb)
 Deploy | [glpi-agent-task-deploy_${TAG}${DEBREV}_all.deb](../../releases/download/$TAG/glpi-agent-task-deploy_${TAG}${DEBREV}_all.deb)
 
+### RPM packages
 DESCRIPTION
+
+for dist in el8 el7 fc34 fc33
+do
+    case $dist in
+        el7)  NAME="RHEL7/CentOS7" ;;
+        el8)  NAME="RHEL8/CentOS8" ;;
+        fc34) NAME="Fedora 34" ;;
+        fc33) NAME="Fedora 33" ;;
+    esac
+    cat >>release-description.md <<RPM
+Distro | Task |Packages
+---|:---
+$NAME | Inventory| [glpi-agent-${TAG}${RPMREV}.$dist.noarch.rpm](../../releases/download/$TAG/glpi-agent-${TAG}${RPMREV}.$dist.noarch.rpm)
+$NAME | NetInventory | [glpi-agent-task-network-${TAG}${RPMREV}.$dist.noarch.rpm](../../releases/download/$TAG/glpi-agent-task-network-${TAG}${RPMREV}.$dist.noarch.rpm)
+$NAME | ESX | [glpi-agent-task-esx-${TAG}${RPMREV}.$dist.noarch.rpm](../../releases/download/$TAG/glpi-agent-task-esx-${TAG}${RPMREV}.$dist.noarch.rpm)
+$NAME | Collect | [glpi-agent-task-collect-${TAG}${RPMREV}.$dist.noarch.rpm](../../releases/download/$TAG/glpi-agent-task-collect-${TAG}${RPMREV}.$dist.noarch.rpm)
+$NAME | Deploy | [glpi-agent-task-deploy-${TAG}${RPMREV}.$dist.noarch.rpm](../../releases/download/$TAG/glpi-agent-task-deploy-${TAG}${RPMREV}.$dist.noarch.rpm)
+$NAME | WakeOnLan | [glpi-agent-task-wakeonlan-${TAG}${RPMREV}.$dist.noarch.rpm](../../releases/download/$TAG/glpi-agent-task-wakeonlan-${TAG}${RPMREV}.$dist.noarch.rpm)
+$NAME | Cron | [glpi-agent-cron-${TAG}${RPMREV}.$dist.noarch.rpm](../../releases/download/$TAG/glpi-agent-cron-${TAG}${RPMREV}.$dist.noarch.rpm)
+
+RPMS
