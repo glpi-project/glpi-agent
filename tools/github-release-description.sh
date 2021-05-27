10309bb445ce2c2401f2c253de81b9f31f26fe9d
Guillaume Bougard <gbougard@teclib.com>
2021-05-27 12:33:40 +0200

    build: use absolute URL in release page as also used in release mail notification

--- tools/github-release-description.sh
+++ tools/github-release-description.sh
@@ -5,6 +5,8 @@ set -e
 TAG="${GITHUB_REF#*refs/tags/}"
 VERSION="$TAG"
 
+: ${REPO:=https://github.com/glpi-project/glpi-agent}
+
 while [ -n "$1" ]
 do
     case "$1" in
@@ -36,54 +38,54 @@ Here you can download GLPI-Agent v$VERSION packages.
 ## Windows
 Arch | Windows installer | Windows portable archive
 ---|:---|:---
-64 bits | [GLPI-Agent-$VERSION-x64.msi](../../releases/download/$TAG/GLPI-Agent-$VERSION-x64.msi) | [glpi-agent-$VERSION-x64.zip](../../releases/download/$TAG/glpi-agent-$VERSION-x64.zip)
-32 bits | [GLPI-Agent-$VERSION-x86.msi](../../releases/download/$TAG/GLPI-Agent-$VERSION-x86.msi) | [glpi-agent-$VERSION-x86.zip](../../releases/download/$TAG/glpi-agent-$VERSION-x86.zip)
+64 bits | [GLPI-Agent-$VERSION-x64.msi]($REPO/releases/download/$TAG/GLPI-Agent-$VERSION-x64.msi) | [glpi-agent-$VERSION-x64.zip]($REPO/releases/download/$TAG/glpi-agent-$VERSION-x64.zip)
+32 bits | [GLPI-Agent-$VERSION-x86.msi]($REPO/releases/download/$TAG/GLPI-Agent-$VERSION-x86.msi) | [glpi-agent-$VERSION-x86.zip]($REPO/releases/download/$TAG/glpi-agent-$VERSION-x86.zip)
 
 ## MacOSX
 
 ### MacOSX - Intel
 Arch | Package
 ---|:---
-x86_64 | PKG: [GLPI-Agent-${VERSION}_x86_64.pkg](../../releases/download/$TAG/GLPI-Agent-${VERSION}_x86_64.pkg)
-x86_64 | DMG: [GLPI-Agent-${VERSION}_x86_64.dmg](../../releases/download/$TAG/GLPI-Agent-${VERSION}_x86_64.dmg)
+x86_64 | PKG: [GLPI-Agent-${VERSION}_x86_64.pkg]($REPO/releases/download/$TAG/GLPI-Agent-${VERSION}_x86_64.pkg)
+x86_64 | DMG: [GLPI-Agent-${VERSION}_x86_64.dmg]($REPO/releases/download/$TAG/GLPI-Agent-${VERSION}_x86_64.dmg)
 
 ### MacOSX - Apple Silicon
 Arch | Package
 ---|:---
-arm64 | PKG: [GLPI-Agent-${VERSION}_arm64.pkg](../../releases/download/$TAG/GLPI-Agent-${VERSION}_arm64.pkg)
-arm64 | DMG: [GLPI-Agent-${VERSION}_arm64.dmg](../../releases/download/$TAG/GLPI-Agent-${VERSION}_arm64.dmg)
+arm64 | PKG: [GLPI-Agent-${VERSION}_arm64.pkg]($REPO/releases/download/$TAG/GLPI-Agent-${VERSION}_arm64.pkg)
+arm64 | DMG: [GLPI-Agent-${VERSION}_arm64.dmg]($REPO/releases/download/$TAG/GLPI-Agent-${VERSION}_arm64.dmg)
 
 ## Linux
 
 ### Linux installer
-Linux installer for redhat/centos/debian/ubuntu (<2Mb):
-[glpi-agent-${VERSION}-linux-installer.pl](../../releases/download/$TAG/glpi-agent-${VERSION}-linux-installer.pl)
+Linux installer for redhat/centos/debian/ubuntu (~2Mb):
+[glpi-agent-${VERSION}-linux-installer.pl]($REPO/releases/download/$TAG/glpi-agent-${VERSION}-linux-installer.pl)
 
 Linux installer for redhat/centos/debian/ubuntu, including snap install support (~20Mb):
-[glpi-agent-${VERSION}-with-snap-linux-installer.pl](../../releases/download/$TAG/glpi-agent-${VERSION}-with-snap-linux-installer.pl)
+[glpi-agent-${VERSION}-with-snap-linux-installer.pl]($REPO/releases/download/$TAG/glpi-agent-${VERSION}-with-snap-linux-installer.pl)
 
 ### Snap package for amd64
-[glpi-agent_${VERSION}_amd64.snap](../../releases/download/$TAG/glpi-agent_${VERSION}_amd64.snap)
+[glpi-agent_${VERSION}_amd64.snap]($REPO/releases/download/$TAG/glpi-agent_${VERSION}_amd64.snap)
 
 ### Debian/Ubuntu packages
-Better use [glpi-agent-${VERSION}-linux-installer.pl](../../releases/download/$TAG/glpi-agent-${VERSION}-linux-installer.pl) when possible.
+Better use [glpi-agent-${VERSION}-linux-installer.pl]($REPO/releases/download/$TAG/glpi-agent-${VERSION}-linux-installer.pl) when possible.
 Related agent task |Package
 ---|:---
-Inventory| [glpi-agent_${VERSION}${DEBREV}_all.deb](../../releases/download/$TAG/glpi-agent_${VERSION}${DEBREV}_all.deb)
-NetInventory | [glpi-agent-task-network_${VERSION}${DEBREV}_all.deb](../../releases/download/$TAG/glpi-agent-task-network_${VERSION}${DEBREV}_all.deb)
-ESX | [glpi-agent-task-esx_${VERSION}${DEBREV}_all.deb](../../releases/download/$TAG/glpi-agent-task-esx_${VERSION}${DEBREV}_all.deb)
-Collect | [glpi-agent-task-collect_${VERSION}${DEBREV}_all.deb](../../releases/download/$TAG/glpi-agent-task-collect_${VERSION}${DEBREV}_all.deb)
-Deploy | [glpi-agent-task-deploy_${VERSION}${DEBREV}_all.deb](../../releases/download/$TAG/glpi-agent-task-deploy_${VERSION}${DEBREV}_all.deb)
+Inventory| [glpi-agent_${VERSION}${DEBREV}_all.deb]($REPO/releases/download/$TAG/glpi-agent_${VERSION}${DEBREV}_all.deb)
+NetInventory | [glpi-agent-task-network_${VERSION}${DEBREV}_all.deb]($REPO/releases/download/$TAG/glpi-agent-task-network_${VERSION}${DEBREV}_all.deb)
+ESX | [glpi-agent-task-esx_${VERSION}${DEBREV}_all.deb]($REPO/releases/download/$TAG/glpi-agent-task-esx_${VERSION}${DEBREV}_all.deb)
+Collect | [glpi-agent-task-collect_${VERSION}${DEBREV}_all.deb]($REPO/releases/download/$TAG/glpi-agent-task-collect_${VERSION}${DEBREV}_all.deb)
+Deploy | [glpi-agent-task-deploy_${VERSION}${DEBREV}_all.deb]($REPO/releases/download/$TAG/glpi-agent-task-deploy_${VERSION}${DEBREV}_all.deb)
 
 ### RPM packages
-RPM packages are arch independents and installation may require some repository setups, better use [glpi-agent-${VERSION}-linux-installer.pl](../../releases/download/$TAG/glpi-agent-${VERSION}-linux-installer.pl) when possible.
+RPM packages are arch independents and installation may require some repository setups, better use [glpi-agent-${VERSION}-linux-installer.pl]($REPO/releases/download/$TAG/glpi-agent-${VERSION}-linux-installer.pl) when possible.
 Task |Packages
 ---|:---
-Inventory| [glpi-agent-${VERSION}${RPMREV}.noarch.rpm](../../releases/download/$TAG/glpi-agent-${VERSION}${RPMREV}.noarch.rpm)
-NetInventory | [glpi-agent-task-network-${VERSION}${RPMREV}.noarch.rpm](../../releases/download/$TAG/glpi-agent-task-network-${VERSION}${RPMREV}.noarch.rpm)
-ESX | [glpi-agent-task-esx-${VERSION}${RPMREV}.noarch.rpm](../../releases/download/$TAG/glpi-agent-task-esx-${VERSION}${RPMREV}.noarch.rpm)
-Collect | [glpi-agent-task-collect-${VERSION}${RPMREV}.noarch.rpm](../../releases/download/$TAG/glpi-agent-task-collect-${VERSION}${RPMREV}.noarch.rpm)
-Deploy | [glpi-agent-task-deploy-${VERSION}${RPMREV}.noarch.rpm](../../releases/download/$TAG/glpi-agent-task-deploy-${VERSION}${RPMREV}.noarch.rpm)
-WakeOnLan | [glpi-agent-task-wakeonlan-${VERSION}${RPMREV}.noarch.rpm](../../releases/download/$TAG/glpi-agent-task-wakeonlan-${VERSION}${RPMREV}.noarch.rpm)
-Cron | [glpi-agent-cron-${VERSION}${RPMREV}.noarch.rpm](../../releases/download/$TAG/glpi-agent-cron-${VERSION}${RPMREV}.noarch.rpm)
+Inventory| [glpi-agent-${VERSION}${RPMREV}.noarch.rpm]($REPO/releases/download/$TAG/glpi-agent-${VERSION}${RPMREV}.noarch.rpm)
+NetInventory | [glpi-agent-task-network-${VERSION}${RPMREV}.noarch.rpm]($REPO/releases/download/$TAG/glpi-agent-task-network-${VERSION}${RPMREV}.noarch.rpm)
+ESX | [glpi-agent-task-esx-${VERSION}${RPMREV}.noarch.rpm]($REPO/releases/download/$TAG/glpi-agent-task-esx-${VERSION}${RPMREV}.noarch.rpm)
+Collect | [glpi-agent-task-collect-${VERSION}${RPMREV}.noarch.rpm]($REPO/releases/download/$TAG/glpi-agent-task-collect-${VERSION}${RPMREV}.noarch.rpm)
+Deploy | [glpi-agent-task-deploy-${VERSION}${RPMREV}.noarch.rpm]($REPO/releases/download/$TAG/glpi-agent-task-deploy-${VERSION}${RPMREV}.noarch.rpm)
+WakeOnLan | [glpi-agent-task-wakeonlan-${VERSION}${RPMREV}.noarch.rpm]($REPO/releases/download/$TAG/glpi-agent-task-wakeonlan-${VERSION}${RPMREV}.noarch.rpm)
+Cron | [glpi-agent-cron-${VERSION}${RPMREV}.noarch.rpm]($REPO/releases/download/$TAG/glpi-agent-cron-${VERSION}${RPMREV}.noarch.rpm)
 DESCRIPTION
