07469cb3da74ad67b83df050e6be121069cdd904
Guillaume Bougard <gbougard@teclib.com>
2021-07-01 19:19:25 +0200

    test: add mariadb mysql test case

--- lib/FusionInventory/Agent/Task/Inventory/Generic/Databases/MySQL.pm
+++ lib/FusionInventory/Agent/Task/Inventory/Generic/Databases/MySQL.pm
@@ -44,7 +44,10 @@ sub _getDatabaseService {
 
     foreach $credential (@credentials) {
         my ($name, $manufacturer) = qw(MySQL Oracle);
-        my $version = _runSql("SHOW VARIABLES LIKE 'version'")
+        my $version = _runSql(
+            sql     => "SHOW VARIABLES LIKE 'version'",
+            %params
+        )
             or next;
         $version =~ s/^version\s*//;
         if ($version =~ /mariadb/i) {
@@ -53,7 +56,10 @@ sub _getDatabaseService {
         }
 
         my $dbs_size = 0;
-        my ($uptime) = _runSql("SHOW GLOBAL STATUS LIKE 'Uptime'") =~ /^Uptime\s(\d+)$/i;
+        my ($uptime) = _runSql(
+                sql => "SHOW GLOBAL STATUS LIKE 'Uptime'",
+                %params
+        ) =~ /^Uptime\s(\d+)$/i;
         my $lastboot = strftime("%F %T", localtime(time - $uptime));
 
         my $dbs = GLPI::Agent::Inventory::DatabaseService->new(
@@ -66,9 +72,14 @@ sub _getDatabaseService {
             last_boot_date  => $lastboot,
         );
 
-        foreach my $db (_runSql("SHOW DATABASES")) {
-            my $size = _runSql("SELECT sum(data_length+index_length) FROM
-                information_schema.TABLES WHERE table_schema = '$db'");
+        foreach my $db (_runSql(
+            sql => "SHOW DATABASES",
+            %params
+        )) {
+            my $size = _runSql(
+                sql => "SELECT sum(data_length+index_length) FROM information_schema.TABLES WHERE table_schema = '$db'",
+                %params
+            );
             if ($size =~ /^\d+$/) {
                 $dbs_size += $size;
                 $size = getCanonicalSize("$size bytes", 1024);
@@ -77,12 +88,16 @@ sub _getDatabaseService {
             }
 
             # Find creation date
-            my $created = _runSql("SELECT MIN(create_time) FROM
-                information_schema.TABLES WHERE table_schema = '$db'");
+            my $created = _runSql(
+                sql => "SELECT MIN(create_time) FROM information_schema.TABLES WHERE table_schema = '$db'",
+                %params
+            );
 
             # Find update date
-            my $updated = _runSql("SELECT MAX(update_time) FROM
-                information_schema.TABLES WHERE table_schema = '$db'");
+            my $updated = _runSql(
+                sql => "SELECT MAX(update_time) FROM information_schema.TABLES WHERE table_schema = '$db'",
+                %params
+            );
 
             $dbs->addDatabase(
                 name            => $db,
@@ -102,14 +117,34 @@ sub _getDatabaseService {
 }
 
 sub _runSql {
-    my ($sql) = @_;
+    my (%params) = @_;
+
+    my $sql = delete $params{sql}
+        or return;
 
     my $options = "";
+    # TODO: update $options to use credentials
     my $command = "mysql $options -q -sN -e \"$sql\"";
-    if (wantarray) {
-        return map { chomp; $_ } getAllLines(command => $command);
+
+    # Only to support unittests
+    if ($params{file}) {
+        $sql =~ s/\s+/-/g;
+        $sql =~ s/[^-_0-9A-Za-z]//g;
+        $sql =~ s/[-][-]+/-/g;
+        $params{file} .= "-" . lc($sql);
+        unless (-e $params{file}) {
+            print STDERR "Generating $params{file} for new MySQL test case...\n";
+            system("$command >$params{file}");
+        }
     } else {
-        my $result  = getAllLines(command => $command);
+        my $options = "";
+        $params{command} = "mysql $options -q -sN -e \"$sql\""
+    }
+
+    if (wantarray) {
+        return map { chomp; $_ } getAllLines(%params);
+    } else {
+        my $result  = getAllLines(%params);
         chomp($result);
         return $result;
     }
---
+++ t/tasks/inventory/generic/databases/mysql.t
@@ -0,0 +1,102 @@
+#!/usr/bin/perl
+
+use strict;
+use warnings;
+use lib 't/lib';
+
+use Data::Dumper;
+
+use Test::Deep;
+use Test::Exception;
+use Test::More;
+use Test::NoWarnings;
+
+use FusionInventory::Test::Inventory;
+use FusionInventory::Agent::Task::Inventory::Generic::Databases::MySQL;
+
+$Data::Dumper::Indent    = 1;
+$Data::Dumper::Terse     = 1;
+$Data::Dumper::Quotekeys = 0;
+$Data::Dumper::Sortkeys  = 1;
+$Data::Dumper::Useqq     = 1;
+$Data::Dumper::Pad       = "    ";
+
+# To add a test case:
+# 1. Add a uniq key as test case name pointing to undef:
+#     'new-test-case' => undef,
+# 2. Run the test
+# 3. From the test output copy the dumped array ref in place of undef
+# All run command will be saved as files under resources/generic/databases so use
+# a still not use test case name
+
+my %db_tests = (
+    'mariadb-10.4.19' => [
+      {
+        DATABASES => [
+          {
+            CREATION_DATE => "2014-09-07 21:50:35",
+            IS_ACTIVE => 1,
+            NAME => "asteriskcdrdb",
+            SIZE => 1,
+            UPDATE_DATE => "2014-09-07 21:50:35"
+          },
+          {
+            CREATION_DATE => "2015-09-29 20:15:36",
+            IS_ACTIVE => 1,
+            NAME => "beacon",
+            SIZE => 0
+          },
+          {
+            CREATION_DATE => "2015-05-19 15:39:23",
+            IS_ACTIVE => 1,
+            NAME => "glpi",
+            SIZE => 52,
+            UPDATE_DATE => "2015-10-01 17:03:02"
+          },
+          {
+            CREATION_DATE => "2021-07-01 18:50:43",
+            IS_ACTIVE => 1,
+            NAME => "information_schema",
+            SIZE => 0,
+            UPDATE_DATE => "2021-07-01 18:50:43"
+          },
+          {
+            CREATION_DATE => "2014-09-07 21:49:47",
+            IS_ACTIVE => 1,
+            NAME => "mysql",
+            SIZE => 0,
+            UPDATE_DATE => "2015-09-29 20:13:04"
+          },
+          {
+            IS_ACTIVE => 1,
+            NAME => "performance_schema",
+            SIZE => 0
+          },
+          {
+            IS_ACTIVE => 1,
+            NAME => "test"
+          }
+        ],
+        MANUFACTURER => "MariaDB",
+        NAME => "MariaDB",
+        SIZE => 55,
+        TYPE => "mysql",
+        VERSION => "10.4.19"
+      }
+    ],
+);
+
+plan tests => (2 * scalar keys %db_tests) + 1;
+
+my $inventory = FusionInventory::Test::Inventory->new();
+
+foreach my $test (keys %db_tests) {
+    my $file  = "resources/generic/databases/$test";
+    my $dbs   = FusionInventory::Agent::Task::Inventory::Generic::Databases::MySQL::_getDatabaseService(file => $file);
+    my $entries = [ map { $_->entry() } @$dbs ];
+    print STDERR "\n$test: ", Dumper($entries) unless defined($db_tests{$test});
+    cmp_deeply($entries, $db_tests{$test}, "$test: parsing");
+    lives_ok {
+        $inventory->addEntry(section => 'DATABASES_SERVICES', entry => $_) foreach @$entries;
+    } "$test: registering";
+}
