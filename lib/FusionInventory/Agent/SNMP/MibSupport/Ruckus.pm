b87712a32187b53ee7f00edbc7042db413099f4b
Guillaume Bougard <gbougard@teclib.com>
2021-07-16 17:22:33 +0200

    feat: Add Ruckus devices support

---
+++ lib/FusionInventory/Agent/SNMP/MibSupport/Ruckus.pm
@@ -0,0 +1,63 @@
+package FusionInventory::Agent::SNMP::MibSupport::Ruckus;
+
+use strict;
+use warnings;
+
+use parent 'FusionInventory::Agent::SNMP::MibSupportTemplate';
+
+use FusionInventory::Agent::Tools;
+use FusionInventory::Agent::Tools::SNMP;
+
+use constant    enterprises => '.1.3.6.1.4.1' ;
+
+# RUCKUS-ROOT-MIB
+use constant    ruckusRootMIB               => enterprises . '.25053' ;
+use constant    ruckusProducts              => ruckusRootMIB . '.3';
+use constant    ruckusCommonHwInfoModule    => ruckusRootMIB . '.1.1.2';
+use constant    ruckusCommonSwInfoModule    => ruckusRootMIB . '.1.1.3';
+
+# RUCKUS-HWINFO-MIB
+use constant    ruckusHwInfo                => ruckusCommonHwInfoModule . '.1.1.1';
+use constant    ruckusHwInfoModelNumber     => ruckusHwInfo . '.1.0';
+use constant    ruckusHwInfoSerialNumber    => ruckusHwInfo . '.2.0';
+
+# RUCKUS-SWINFO-MIB
+use constant    ruckusSwInfo                => ruckusCommonSwInfoModule . '.1.1.1';
+use constant    ruckusSwRevision            => ruckusSwInfo . '.1.1.3.1';
+
+our $mibSupport = [
+    {
+        name        => "ruckus",
+        sysobjectid => getRegexpOidMatch(ruckusProducts)
+    }
+];
+
+sub getModel {
+    my ($self) = @_;
+
+    return $self->get(ruckusHwInfoModelNumber);
+}
+
+sub getSerial {
+    my ($self) = @_;
+
+    return $self->get(ruckusHwInfoSerialNumber);
+}
+
+sub getFirmware {
+    my ($self) = @_;
+
+    return $self->get(ruckusSwRevision);
+}
+
+1;
+
+__END__
+
+=head1 NAME
+
+FusionInventory::Agent::SNMP::MibSupport::Ruckus - Inventory module for Ruckus devices
+
+=head1 DESCRIPTION
+
+The module enhances Ruckus devices support.
