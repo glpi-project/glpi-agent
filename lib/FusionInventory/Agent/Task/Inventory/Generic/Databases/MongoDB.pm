10a0b4a1850a51efcba2f7f4a659dc6bb6a5ea3e
Guillaume Bougard <gbougard@teclib.com>
2021-07-26 18:48:05 +0200

    feat: Add MongoDB database inventory support

---
+++ lib/FusionInventory/Agent/Task/Inventory/Generic/Databases/MongoDB.pm
@@ -0,0 +1,194 @@
+package FusionInventory::Agent::Task::Inventory::Generic::Databases::MongoDB;
+
+use English qw(-no_match_vars);
+
+use strict;
+use warnings;
+
+use parent 'FusionInventory::Agent::Task::Inventory::Generic::Databases';
+
+use POSIX qw(strftime);
+
+use FusionInventory::Agent::Tools;
+use GLPI::Agent::Inventory::DatabaseService;
+
+sub isEnabled {
+    return canRun('mongo') || canRun('mongosh');
+}
+
+sub doInventory {
+    my (%params) = @_;
+
+    my $inventory = $params{inventory};
+
+    # Try to retrieve credentials updating params
+    FusionInventory::Agent::Task::Inventory::Generic::Databases::_credentials(\%params, "mongodb");
+
+    my $dbservices = _getDatabaseService(%params);
+
+    foreach my $dbs (@{$dbservices}) {
+        $inventory->addEntry(
+            section => 'DATABASES_SERVICES',
+            entry   => $dbs->entry(),
+        );
+    }
+}
+
+sub _getDatabaseService {
+    my (%params) = @_;
+
+    my $credentials = delete $params{credentials};
+    return [] unless $credentials && ref($credentials) eq 'ARRAY';
+
+    my @dbs = ();
+
+    $params{mongosh} = canRun('mongosh') ? 1 : 0;
+
+    foreach my $credential (@{$credentials}) {
+        my $rcfile = _mongoRcFile($credential);
+        $params{rcfile} = $rcfile->filename if $rcfile;
+
+        my ($name, $manufacturer) = qw(MongoDB MongoDB);
+        my $version = _runSql(
+            sql     => "db.version()",
+            nodb    => 1,
+            %params
+        )
+            or next;
+
+        my $dbs_size = 0;
+        my $lastbootmilli = _runSql(
+            sql => "ISODate().getTime()-db.serverStatus().uptimeMillis",
+            %params
+        );
+        my $lastboot;
+        $lastboot = strftime("%F %T", localtime(int($lastbootmilli/1000)))
+            if $lastbootmilli;
+
+        my $dbs = GLPI::Agent::Inventory::DatabaseService->new(
+            type            => "mongodb",
+            name            => $name,
+            version         => $version,
+            manufacturer    => $manufacturer,
+            port            => $credential->{port} // 27017,
+            is_active       => 1,
+            last_boot_date  => $lastboot,
+        );
+
+        my @databases = _runSql(
+            sql => "db.adminCommand( { listDatabases: 1 } ).databases",
+            %params
+        );
+
+        foreach my $dbinfo (@databases) {
+            my ($db, $size) = $dbinfo =~ /\{\s*name:\s*'([^']+)',\s*sizeOnDisk:\s*Long\("(\d+)"\)/
+                or next;
+
+            if ($size) {
+                $dbs_size += $size;
+                $size = getCanonicalSize("$size bytes", 1024);
+            } else {
+                undef $size;
+            }
+
+            my $status = _runSql(
+                sql => "db = new Mongo().getDB('$db');db.runCommand('ping').ok",
+                %params
+            );
+
+            $dbs->addDatabase(
+                name            => $db,
+                size            => $size,
+                is_active       => $status,
+            );
+        }
+
+        $dbs->size(getCanonicalSize("$dbs_size bytes", 1024));
+
+        push @dbs, $dbs;
+
+        # Always forget rcfile
+        delete $params{rcfile};
+    }
+
+    return \@dbs;
+}
+
+sub _date {
+    my ($date) = @_
+        or return;
+    $date =~ /^(\d{4}-\d{2}-\d{2}\s+\d{2}:\d{2}:\d{2})/;
+    return $1;
+}
+
+sub _runSql {
+    my (%params) = @_;
+
+    my $sql = delete $params{sql}
+        or return;
+
+    my $nodb = delete $params{norc};
+    my $rcfile = delete $params{rcfile};
+    my $command = "mongo";
+    $command .= "sh" if $params{mongosh};
+    $command .= " --quiet";
+    $command .= " --nodb" if $nodb;
+    $command .= " --norc $rcfile" if $rcfile;
+    $command .= " --eval \"$sql\"";
+
+    # Only to support unittests
+    if ($params{file}) {
+        $sql =~ s/[ .]+/-/g;
+        $sql =~ s/[^-_0-9A-Za-z]//g;
+        $sql =~ s/[-][-]+/-/g;
+        $params{file} .= "-" . lc($sql);
+        unless ($params{istest}) {
+            print STDERR "\nGenerating $params{file} for new MongoDB test case...\n";
+            system("$command >$params{file}");
+        }
+    } else {
+        $params{command} = $command;
+    }
+
+    if (wantarray) {
+        return map { chomp; $_ } getAllLines(%params);
+    } else {
+        my $result  = getFirstLine(%params);
+        return unless defined($result);
+        chomp($result);
+        return $result;
+    }
+}
+
+sub _mongoRcFile {
+    my ($credential) = @_;
+
+    return unless $credential->{type};
+
+    my $fh;
+    if ($credential->{type} eq "login_password") {
+        File::Temp->require();
+
+        $fh = File::Temp->new(
+            TEMPLATE    => '.mongorc-XXXXXX',
+            SUFFIX      => '.js',
+        );
+        my $conn = $credential->{host} // "localhost";
+        $conn .= ":".($credential->{port} // "27017");
+        print $fh "conn = new Mongo(\"$conn\");\n";
+        $conn = $credential->{socket} ? '"'.$credential->{socket}.'"' : "";
+        print $fh "db = connect($conn);\n";
+        if ($credential->{login} && $credential->{password}) {
+            print $fh "db.auth({\n";
+            print $fh "    user: '$credential->{login}',\n";
+            print $fh "    pwd: '$credential->{password}',\n";
+            print $fh "});\n";
+        }
+        close($fh);
+    }
+
+    # Temp file must be deleted out of caller scope
+    return $fh;
+}
+
+1;
---
+++ t/tasks/inventory/generic/databases/mongodb.t
@@ -0,0 +1,86 @@
+#!/usr/bin/perl
+
+use strict;
+use warnings;
+use lib 't/lib';
+
+use Data::Dumper;
+
+use Test::Deep;
+use Test::Exception;
+use Test::More;
+use Test::NoWarnings;
+
+use FusionInventory::Test::Inventory;
+use FusionInventory::Agent::Task::Inventory::Generic::Databases::MongoDB;
+
+$Data::Dumper::Indent    = 1;
+$Data::Dumper::Terse     = 1;
+$Data::Dumper::Quotekeys = 0;
+$Data::Dumper::Sortkeys  = 1;
+$Data::Dumper::Useqq     = 1;
+$Data::Dumper::Pad       = "    ";
+
+# To add a test case:
+# 1. Add a uniq key as test case name pointing to undef:
+#     'new-test-case' => undef,
+# 2. Run the test
+# 3. From the test output copy the dumped array ref in place of undef
+# All run command will be saved as files under resources/generic/databases so use
+# a still not use test case name
+
+my %db_tests = (
+    'nodb'            => [],
+    'mongodb-5.0.1' => [
+      {
+        DATABASES => [
+          {
+            IS_ACTIVE => 1,
+            NAME => "admin",
+            SIZE => 0
+          },
+          {
+            IS_ACTIVE => 1,
+            NAME => "config",
+            SIZE => 0
+          },
+          {
+            IS_ACTIVE => 1,
+            NAME => "local",
+            SIZE => 0
+          }
+        ],
+        LAST_BOOT_DATE => "2021-07-26 11:42:13",
+        MANUFACTURER => "MongoDB",
+        NAME => "MongoDB",
+        PORT => 27017,
+        SIZE => 0,
+        TYPE => "mongodb",
+        VERSION => "5.0.1"
+      }
+    ],
+);
+
+my %credentials = (
+    # Set related credentials values when needed like port for a given test or
+    # to generate the test case but don't forget to mask any sensible data after
+);
+
+plan tests => (2 * scalar keys %db_tests) + 1;
+
+my $inventory = FusionInventory::Test::Inventory->new();
+
+foreach my $test (keys %db_tests) {
+    my $file  = "resources/generic/databases/$test";
+    my $dbs   = FusionInventory::Agent::Task::Inventory::Generic::Databases::MongoDB::_getDatabaseService(
+        file        => $file,
+        credentials => $credentials{$test} // [{}],
+        istest      => $db_tests{$test} ? 1 : 0,
+    );
+    my $entries = [ map { $_->entry() } @$dbs ];
+    print STDERR "\n$test: ", Dumper($entries) unless defined($db_tests{$test});
+    cmp_deeply($entries, $db_tests{$test}, "$test: parsing");
+    lives_ok {
+        $inventory->addEntry(section => 'DATABASES_SERVICES', entry => $_) foreach @$entries;
+    } "$test: registering";
+}
