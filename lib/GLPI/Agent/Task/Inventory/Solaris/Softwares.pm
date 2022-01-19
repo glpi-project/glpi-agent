410df872e5da7d194cd014986ba72f7b22872702
Guillaume Bougard <gbougard@teclib.com>
2022-01-19 14:21:49 +0100

    feat: Update Solaris software inventory adding install date and file size support

--- lib/GLPI/Agent/Task/Inventory/Solaris/Softwares.pm
+++ lib/GLPI/Agent/Task/Inventory/Solaris/Softwares.pm
@@ -5,6 +5,8 @@ use warnings;
 
 use parent 'GLPI::Agent::Task::Inventory::Module';
 
+use UNIVERSAL::require;
+
 use GLPI::Agent::Tools;
 
 use constant    category    => "software";
@@ -44,6 +46,8 @@ sub _parse_pkgs {
     my $handle = getFileHandle(%params);
     return unless $handle;
 
+    my %month = qw(Jan 1 Feb 2 Mar 3 Apr 4 May 5 Jun 6 Jul 7 Aug 8 Sep 9 Oct 10 Nov 11 Dec 12);
+
     my @softwares;
     my $software;
     if ($params{command} =~ /pkg info/) {
@@ -59,6 +63,21 @@ sub _parse_pkgs {
                 $software->{PUBLISHER} = $1;
             } elsif ($line =~  /Summary:\s+(.+)/) {
                 $software->{COMMENTS} = $1;
+            } elsif ($line =~  /Last Install Time:\s+\S+\s+(\S+)\s+(\d+)\s+\S+\s+(\d+)$/) {
+                if (DateTime->require) {
+                    my $date;
+                    eval {
+                        $date = DateTime->new(
+                            month   => $month{$1},
+                            day     => $2,
+                            year    => $3,
+                        );
+                    };
+                    $software->{INSTALLDATE} = $date->dmy('/') if $date;
+                }
+            } elsif ($line =~  /Size:\s+(.+)$/) {
+                my $size = getCanonicalSize($1, 1024);
+                $software->{FILESIZE} = int($size) if defined($size);
             }
         }
     } else {
@@ -74,6 +93,18 @@ sub _parse_pkgs {
                 $software->{PUBLISHER} = $1;
             } elsif ($line =~  /DESC:\s+(.+)/) {
                 $software->{COMMENTS} = $1;
+            } elsif ($line =~  /INSTDATE:\s+(\S+)\s+(\d+)\s+(\d+)/) {
+                if (DateTime->require) {
+                    my $date;
+                    eval {
+                        $date = DateTime->new(
+                            month   => $month{$1},
+                            day     => $2,
+                            year    => $3,
+                        );
+                    };
+                    $software->{INSTALLDATE} = $date->dmy('/') if $date;
+                }
             }
         }
     }
