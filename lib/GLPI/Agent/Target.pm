glpi-project/glpi-agent

commit fd6b450a9f02dbe8985966f299758edd7912864e

fix: cleanup glpi server support in target api

lib/GLPI/Agent/Target.pm
@@ -300,7 +300,9 @@ sub _loadState {
 
     $self->{maxDelay}    = $data->{maxDelay}    if $data->{maxDelay};
     $self->{nextRunDate} = $data->{nextRunDate} if $data->{nextRunDate};
-    $self->{_type}       = $data->{_type}       if $data->{_type};
+
+    # Update us as GLPI server is recognized as so before
+    $self->isGlpiServer(1) if $data->{is_glpi_server};
 
     # Disable initialDelay if next run date has still been set in a previous run
     delete $self->{initialDelay} if $data->{nextRunDate};
@@ -312,13 +314,10 @@ sub _saveState {
     my $data ={
         maxDelay    => $self->{maxDelay},
         nextRunDate => $self->{nextRunDate},
-        type        => $self->getType(),
-        path        => $self->getType() eq 'local'  ? $self->getPath() : '',
-        url         => $self->getType() eq 'server' ? $self->getUrl()  : '',
-        id          => $self->{id},
     };
 
-    $data->{_type} = $self->{_type} if defined($self->{_type});
+    # Add a flag if we are a GLPI server target
+    $data->{is_glpi_server} = 1 if $self->isGlpiServer();
 
     $self->{storage}->save(
         name => 'target',

commit 6c50bec1707cbde3eca7d87343447839cc6356c1

fix: fix glpi-remote as it was broken in fd6b450a9f02dbe8985966f299758edd7912864e

lib/GLPI/Agent/Target.pm
@@ -302,8 +302,9 @@ sub _loadState {
 
     my $data = $self->{storage}->restore(name => 'target');
 
-    $self->{maxDelay}    = $data->{maxDelay}    if $data->{maxDelay};
-    $self->{nextRunDate} = $data->{nextRunDate} if $data->{nextRunDate};
+    map { $self->{$_} = $data->{$_} } grep { defined($data->{$_}) } qw/
+        maxDelay nextRunDate id
+    /;
 
     # Update us as GLPI server is recognized as so before
     $self->isGlpiServer(1) if $data->{is_glpi_server};
@@ -315,10 +316,20 @@ sub _saveState {
     my $data ={
         maxDelay    => $self->{maxDelay},
         nextRunDate => $self->{nextRunDate},
+        type        => $self->getType(),                 # needed by glpi-remote
+        id          => $self->id(),                      # needed by glpi-remote
     };
 
-    # Add a flag if we are a GLPI server target
-    $data->{is_glpi_server} = 1 if $self->isGlpiServer();
+    if ($self->isType('server')) {
+        # Add a flag if we are a GLPI server target
+        $data->{is_glpi_server} = 1 if $self->isGlpiServer();
+        my $url = $self->getUrl();
+        if (ref($url) =~ /^URI/) {
+            $data->{url} = $url->as_string;              # needed by glpi-remote
+        }
+    } elsif ($self->isType('local')) {
+        $data->{path} = $self->getPath();                # needed by glpi-remote
+    }
 
     $self->{storage}->save(
         name => 'target',