bfc530075706ab83fc98f7c388da001315248567
Guillaume Bougard <gbougard@teclib.com>
2022-02-04 10:08:45 +0100

    doc: Changes updates for GLPI 1.1

--- lib/GLPI/Agent/Task/NetInventory/Version.pm
+++ lib/GLPI/Agent/Task/NetInventory/Version.pm
@@ -3,6 +3,6 @@ package GLPI::Agent::Task::NetInventory::Version;
 use strict;
 use warnings;
 
-use constant VERSION => "4.4";
+use constant VERSION => "5.0";
 
 1;
--- lib/GLPI/Agent/Task/RemoteInventory/Version.pm
+++ lib/GLPI/Agent/Task/RemoteInventory/Version.pm
@@ -3,6 +3,6 @@ package GLPI::Agent::Task::RemoteInventory::Version;
 use strict;
 use warnings;
 
-use constant VERSION => "0.9";
+use constant VERSION => "1.0";
 
 1;
