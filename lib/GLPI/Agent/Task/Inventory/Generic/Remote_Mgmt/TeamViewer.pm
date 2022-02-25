92364eadd94c116655d246e1b1eb43caf0175bd9
Guillaume Bougard <gbougard@teclib.com>
2022-02-25 15:52:18 +0100

    fix: teamviewer remote_mgmt inventory regression

--- lib/GLPI/Agent/Task/Inventory/Generic/Remote_Mgmt/TeamViewer.pm
+++ lib/GLPI/Agent/Task/Inventory/Generic/Remote_Mgmt/TeamViewer.pm
@@ -51,7 +51,10 @@ sub doInventory {
     my $inventory = $params{inventory};
     my $logger    = $params{logger};
 
-    my $teamViewerID = _getID(logger => $logger);
+    my $teamViewerID = _getID(
+        osname  => OSNAME,
+        logger  => $logger
+    );
     if (defined($teamViewerID)) {
         $logger->debug('Found TeamViewerID : ' . $teamViewerID) if ($logger);
 
@@ -70,51 +73,70 @@ sub doInventory {
 sub _getID {
     my (%params) = @_;
 
-    if (OSNAME eq 'MSWin32') {
+    my $osname = delete $params{osname} // '';
 
-        GLPI::Agent::Tools::Win32->use();
+    return _getID_MSWin32() if $osname eq "MSWin32";
+    return _getID_darwin(%params)  if $osname eq "darwin";
 
-        my $clientid = getRegistryKeyValue(
-            path => "HKEY_LOCAL_MACHINE/SOFTWARE/TeamViewer/ClientID",
+    return _getID_teamviewer_info(%params);
+}
+
+sub _getID_MSWin32 {
+
+    GLPI::Agent::Tools::Win32->use();
+
+    my $clientid = getRegistryValue(
+        path => "HKEY_LOCAL_MACHINE/SOFTWARE/TeamViewer/ClientID",
+    );
+    if (!$clientid && is64bit()) {
+        $clientid = getRegistryValue(
+            path => "HKEY_LOCAL_MACHINE/SOFTWARE/Wow6432Node/TeamViewer/ClientID",
         );
-        if (!$clientid && is64bit()) {
-            $clientid = getRegistryKeyValue(
-                path => "HKEY_LOCAL_MACHINE/SOFTWARE/Wow6432Node/TeamViewer/ClientID",
-            );
-        }
+    }
 
-        unless ($clientid) {
-            my $teamviever_reg = getRegistryKey(
-                path => "HKEY_LOCAL_MACHINE/SOFTWARE/TeamViewer",
+    unless ($clientid) {
+        my $teamviever_reg = getRegistryKey(
+            path => "HKEY_LOCAL_MACHINE/SOFTWARE/TeamViewer",
+            # Important for remote inventory optimization
+            required    => [ qw/ClientID/ ],
+        );
+        if(!$teamviever_reg && is64bit()){
+            $teamviever_reg = getRegistryKey(
+                path => "HKEY_LOCAL_MACHINE/SOFTWARE/Wow6432Node/TeamViewer",
                 # Important for remote inventory optimization
                 required    => [ qw/ClientID/ ],
             );
-            if(!$teamviever_reg && is64bit()){
-                $teamviever_reg = getRegistryKey(
-                    path => "HKEY_LOCAL_MACHINE/SOFTWARE/Wow6432Node/TeamViewer",
-                    # Important for remote inventory optimization
-                    required    => [ qw/ClientID/ ],
-                );
-            }
-
-            # Look for subkey beginning with Version
-            foreach my $key (keys(%{$teamviever_reg})) {
-                next unless $key =~ /^Version\d+\//;
-                $clientid = $teamviever_reg->{$key}->{"/ClientID"};
-                last if (defined($clientid));
-            }
         }
 
-        return hex2dec($clientid);
+        return unless $teamviever_reg;
+
+        # Look for subkey beginning with Version
+        foreach my $key (keys(%{$teamviever_reg})) {
+            next unless $key =~ /^Version\d+\//;
+            $clientid = $teamviever_reg->{$key}->{"/ClientID"};
+            last if (defined($clientid));
+        }
     }
 
-    if (OSNAME eq 'darwin') {
-        my ( $plist_file ) = Glob(
-            "/Library/Preferences/com.teamviewer.teamviewer*.plist"
-        );
+    return hex2dec($clientid);
+}
 
-        return getFirstLine( command => "defaults read $plist_file ClientID" ) ;
-    }
+sub _getID_darwin {
+    my (%params) = @_;
+
+    # Use $params{darwin_glob} & $params{file} for tests
+    my ( $plist_file ) = $params{darwin_glob} || Glob(
+        "/Library/Preferences/com.teamviewer.teamviewer*.plist"
+    );
+
+    return getFirstLine(
+        command => "defaults read $plist_file ClientID",
+        %params
+    );
+}
+
+sub _getID_teamviewer_info {
+    my (%params) = @_;
 
     return getFirstMatch(
         command => "teamviewer --info",
---
+++ t/tasks/inventory/generic/remote_mgmt/teamviewer.t
@@ -0,0 +1,60 @@
+#!/usr/bin/perl
+
+use strict;
+use warnings;
+
+use lib 't/lib';
+
+use English qw(-no_match_vars);
+use Test::Deep qw(cmp_deeply);
+use Test::More;
+use Test::MockModule;
+use Test::NoWarnings;
+
+use GLPI::Test::Utils;
+
+use GLPI::Agent::Task::Inventory::Generic::Remote_Mgmt::TeamViewer;
+
+my %teamviewer_win32 = (
+    '14.1.3399' => "660666566",
+    '15.27.3'   => "881955027",
+);
+
+my %teamviewer_info = (
+    '15.11.6-RPM' => "999"
+);
+
+plan tests => scalar(keys %teamviewer_win32) + scalar(keys %teamviewer_info) + 1;
+
+my $module = Test::MockModule->new(
+    'GLPI::Agent::Tools::Win32'
+);
+
+foreach my $test (keys %teamviewer_win32) {
+    $module->mock(
+        '_getRegistryKey',
+        _mockGetRegistryKey($test)
+    );
+    my $teamViewerID = GLPI::Agent::Task::Inventory::Generic::Remote_Mgmt::TeamViewer::_getID(osname => "MSWin32");
+    is($teamViewerID, $teamviewer_win32{$test}, "TeamViewer win32 getID - $test");
+}
+
+foreach my $test (sort keys %teamviewer_info) {
+    my $file = "resources/generic/teamviewer/teamviewer-info-$test";
+    my $teamViewerID = GLPI::Agent::Task::Inventory::Generic::Remote_Mgmt::TeamViewer::_getID(file => $file, osname => "linux");
+    is($teamViewerID, $teamviewer_info{$test}, "TeamViewer info getID - $test");
+}
+
+# Adapted from GLPI::Test::Utils mockGetRegistryKey()
+sub _mockGetRegistryKey {
+    my ($test) = @_;
+
+    return sub {
+        my (%params) = @_;
+
+        # We can mock getRegistryKey or better _getRegistryKey to cover getRegistryValue
+        my $path = $params{path} || $params{keyName};
+        my $file = "resources/generic/teamviewer/teamviewer-$test-$path.reg";
+        return loadRegistryDump($file);
+    };
+}
