b46fb29d33aba416226f6350dc9b48a06af6a49f
root <miguelangelrtorresco@gmail.com>
2022-07-05 23:10:17 -0700

    Following comments related.

--- lib/GLPI/Agent/Task/Inventory/Generic/Remote_Mgmt/MeshCentral.pm
+++ lib/GLPI/Agent/Task/Inventory/Generic/Remote_Mgmt/MeshCentral.pm
@@ -10,17 +10,29 @@ use English qw(-no_match_vars);
 use GLPI::Agent::Tools;
 
 sub isEnabled {
-    return OSNAME eq 'MSWin32' ?
-        canRun("C:\\Program Files\\Mesh Agent\\MeshAgent.exe") :
-        canRun('/usr/local/mesh_services/meshagent/meshagent');
-}
+    my (%params) = @_;
+    my $logger    = $params{logger};
 
-sub _getNodeId {
-    return OSNAME eq 'MSWin32' ?
-    getFirstLine(
-        command => 'C:\\Program Files\\Mesh Agent\\MeshAgent.exe -nodeid') :
-    getFirstLine(
-        command => '/usr/local/mesh_services/meshagent/meshagent -nodeid');
+    if (OSNAME eq 'MSWin32') {
+
+        GLPI::Agent::Tools::Win32->use();
+
+        my $key = getRegistryKey(
+            path        => "HKEY_LOCAL_MACHINE/SOFTWARE/Open Source/Mesh Agent",
+            required    => [ qw/NodeId/ ],
+            maxdepth    => 1,
+            logger      => $params{logger}
+        );
+
+        return $key && (keys %$key);
+
+    } elsif (OSNAME eq 'darwin') {
+        return canRun('defaults') && Glob(
+            "/Library/LaunchDaemons/meshagent*.plist"
+        );
+    }
+
+    return Glob('/etc/systemd/system/meshagent.service');
 }
 
 sub doInventory {
@@ -29,9 +41,8 @@ sub doInventory {
     my $inventory = $params{inventory};
     my $logger    = $params{logger};
 
-    my $nodeId = _getNodeId();
-
-    return unless $nodeId;
+    my $nodeId = _getNodeId()
+        or return;
 
     $inventory->addEntry(
         section => 'REMOTE_MGMT',
@@ -39,6 +50,46 @@ sub doInventory {
             ID   => $nodeId,
             TYPE => 'meshcentral'
         }
+    );
+}
+
+sub _getNodeId {
+    my (%params) = @_;
+
+    return _winBased() if OSNAME eq 'MSWin32';
+    return _darwinBased() if OSNAME eq 'darwin';
+    return _linuxBased();
+}
+
+sub _winBased {
+    my (%params) = @_;
+
+    my $nodeId = getRegistryValue(
+        path        => "HKEY_LOCAL_MACHINE/SOFTWARE/Open Source/Mesh Agent/NodeId",
+        logger      => $params{logger}
+    );
+
+    return $nodeId; 
+}
+
+sub _linuxBased {
+    my (%params) = @_;    
+
+    my $command = getFirstLine(
+            file    => "/etc/systemd/system/meshagent.service",
+            pattern => qr/Ex.*=(.*)\s\-/,
+            logger  => $params{logger},
+    );
+
+    return getFirstLine(
+        command => "${command} -nodeid");
+}
+
+sub _darwinBased {
+    my (%params) = @_;    
+
+    return getFirstLine(
+        command => "/usr/local/mesh_services/meshagent/meshagent_osx64 -nodeid",
     );
 }
 
