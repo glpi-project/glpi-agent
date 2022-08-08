9ac9783d0a51376bb0a51d5264142dfddbd44286
Paolo Asperti <paspo@users.noreply.github.com>
2022-08-08 18:46:25 +0200

    Apply suggestions from code review
    Co-authored-by: Guillaume Bougard <gbougard@teclib.com>

--- lib/GLPI/Agent/Task/Inventory/Generic/Remote_Mgmt/RustDesk.pm
+++ lib/GLPI/Agent/Task/Inventory/Generic/Remote_Mgmt/RustDesk.pm
@@ -12,19 +12,13 @@ use English qw(-no_match_vars);
 use GLPI::Agent::Tools;
 
 sub _get_rustdesk_config {
-    my @configs = ();
-    if (OSNAME eq 'MSWin32') {
-        if (has_folder('C:\Windows\ServiceProfiles\LocalService\AppData\Roaming\RustDesk')) {
-            push @configs, 'C:\Windows\ServiceProfiles\LocalService\AppData\Roaming\RustDesk\config\RustDesk.toml';
-        }
-    } else {
-        push @configs, '/root/.config/rustdesk/RustDesk.toml';
-    }
-    return grep { has_file($_) } @configs;
+    return OSNAME eq 'MSWin32' ?
+        'C:\Windows\ServiceProfiles\LocalService\AppData\Roaming\RustDesk\config\RustDesk.toml' :
+        '/root/.config/rustdesk/RustDesk.toml';
 }
 
 sub isEnabled {
-    return _get_rustdesk_config() ? 1 : 0;
+    return has_file(_get_rustdesk_config());
 }
 
 sub doInventory {
@@ -33,26 +27,25 @@ sub doInventory {
     my $inventory = $params{inventory};
     my $logger    = $params{logger};
 
-    foreach my $conf (_get_rustdesk_config()) {
-        my $RustDeskID = getFirstMatch(
-            file    => $conf,
-            logger  => $logger,
-            pattern => qr/^id\s*=\s*'(.*)'$/
+    my $conf = _get_rustdesk_config();
+    my $RustDeskID = getFirstMatch(
+        file    => $conf,
+        logger  => $logger,
+        pattern => qr/^id\s*=\s*'(.*)'$/
+    );
+
+    if (defined($RustDeskID)) {
+        $logger->debug('Found RustDesk ID : ' . $RustDeskID) if $logger;
+
+        $inventory->addEntry(
+            section => 'REMOTE_MGMT',
+            entry   => {
+                ID   => $RustDeskID,
+                TYPE => 'rustdesk'
+            }
         );
-
-        if (defined($RustDeskID)) {
-            $logger->debug('Found RustDesk ID : ' . $RustDeskID) if $logger;
-
-            $inventory->addEntry(
-                section => 'REMOTE_MGMT',
-                entry   => {
-                    ID   => $RustDeskID,
-                    TYPE => 'rustdesk'
-                }
-            );
-        } else {
-            $logger->debug('RustDesk ID not found in '.$conf) if $logger;
-        }
+    } else {
+        $logger->debug('RustDesk ID not found in '.$conf) if $logger;
     }
 }
 
