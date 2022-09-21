ed8b989ec0d00ae7d922a4269365409b4ed66e5b
Guillaume Bougard <gbougard@teclib.com>
2022-09-21 19:18:24 +0200

    build: Add XML::LibXML to windows build

--- contrib/windows/packaging/PerlBuildJob.pm
+++ contrib/windows/packaging/PerlBuildJob.pm
@@ -156,7 +156,7 @@ sub _build_steps {
 
                 # GLPI-Agent deps
                 qw/ Text::Template UNIVERSAL::require UNIVERSAL::isa Net::SSH2
-                    XML::TreePP XML::XPath Memoize Time::HiRes Compress::Zlib
+                    XML::TreePP XML::XPath XML::LibXML Memoize Time::HiRes Compress::Zlib
                     Parse::EDID Cpanel::JSON::XS JSON::PP YAML::Tiny Parallel::ForkManager
                     URI::Escape Net::NBName Thread::Queue Thread::Semaphore
                     Net::SNMP Net::SNMP::Security::USM Net::SNMP::Transport::IPv4::TCP
