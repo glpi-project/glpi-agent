33038d19fd0426ce086097aa532ba7e760e3ae6e
Guillaume Bougard <gbougard@teclib.com>
2022-10-14 19:46:04 +0200

    build: Update zlib to 1.2.13 for MacOSX builds

--- contrib/macosx/glpi-agent-packaging.sh
+++ contrib/macosx/glpi-agent-packaging.sh
@@ -5,8 +5,8 @@
 # ZLIB: https://www.zlib.net/
 : ${PERL_VERSION:=5.36.0}
 : ${OPENSSL_VERSION:=3.0.5}
-: ${ZLIB_VERSION:=1.2.12}
-: ${ZLIB_SHA256:=91844808532e5ce316b3c010929493c0244f3d37593afd6de04f71821d5136d9}
+: ${ZLIB_VERSION:=1.2.13}
+: ${ZLIB_SHA256:=b3a24de97a8fdbc835b9833169501030b8977031bcb54b3b3ac13740f846ab30}
 
 : ${BUILDER_NAME:="Guillaume Bougard (teclib)"}
 : ${BUILDER_MAIL:="gbougard_at_teclib.com"}
