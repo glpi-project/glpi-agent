f346862e2ad24ca07ede0bcd1b5c76c8f7201847
Guillaume Bougard <gbougard@teclib.com>
2022-11-15 16:03:05 +0100

    refacto: Move netdiscovery queue logic in related job class

--- lib/GLPI/Agent/Task/NetDiscovery/Job.pm
+++ lib/GLPI/Agent/Task/NetDiscovery/Job.pm
@@ -5,6 +5,8 @@ use warnings;
 
 use English qw(-no_match_vars);
 
+use Net::IP;
+
 use GLPI::Agent::Logger;
 
 sub new {
@@ -35,8 +37,121 @@ sub max_threads {
     return $self->{_params}->{THREADS_DISCOVERY} || 1;
 }
 
+sub getQueueParams {
+    my ($self, $range) = @_;
+
+    my $start = $range->{start};
+    my $end   = $range->{end};
+
+    my $block = Net::IP->new( "$start-$end" );
+    if (!$block || !$block->ip() || $block->{binip} !~ /1/) {
+        $self->{logger}->error(
+            "IPv4 range not supported by Net::IP: $start-$end"
+        );
+        return 0;
+    }
+
+    unless ($block->size()) {
+        $self->{logger}->error("Skipping empty range: $start-$end");
+        return 0;
+    }
+
+    $self->{logger}->debug("initializing block $start-$end");
+
+    $range->{block} = $block;
+
+    my $params = {
+        size    => $block->size()->numify(),
+        range   => $range
+    };
+
+    return 1, $params;
+}
+
+sub updateQueue {
+    my ($self, %params) = @_;
+
+    $self->{_queue}->{size} += $params{size};
+    push @{$self->{_queue}->{ranges}}, $params{range} if $params{range};
+}
+
+sub queuesize {
+    my ($self) = @_;
+
+    return 0 unless $self->{_queue};
+
+    return $self->{_queue}->{size} // 0;
+}
+
+sub started {
+    my ($self) = @_;
+
+    return 0 unless $self->{_queue};
+
+    return 1 if $self->{_queue}->{started};
+
+    # Be sure to return true next time
+    $self->{_queue}->{started}++;
+    return 0;
+}
+
+sub done {
+    my ($self) = @_;
+
+    return 0 unless $self->{_queue};
+
+    $self->{_queue}->{in_queue} --;
+    $self->{_queue}->{done} ++;
+
+    return $self->{_queue}->{done} >= $self->{_queue}->{size} ? 1 : 0;
+}
+
+sub max_in_queue {
+    my ($self) = @_;
+
+    return 0 unless $self->{_queue};
+
+    return $self->{_queue}->{in_queue} >= $self->max_threads() ? 1 : 0;
+}
+
+sub range {
+    my ($self) = @_;
+
+    return unless $self->{_queue};
+
+    return $self->{_queue}->{ranges}->[0];
+}
+
+sub nextip {
+    my ($self) = @_;
+
+    return unless $self->{_queue};
+
+    my $range = $self->{_queue}->{ranges}->[0];
+    my $block = $range->{block};
+    my $blockip = $block->ip();
+    # Still update block and handle range list
+    $range->{block} = $block + 1;
+    shift @{$self->{_queue}->{ranges}} unless $range->{block};
+
+    $self->{_queue}->{in_queue}++ if $blockip;
+
+    return $blockip;
+}
+
 sub ranges {
     my ($self) = @_;
+
+    # After _queue has been defined, return the queue ranges count
+    return scalar(@{$self->{_queue}->{ranges}}) if $self->{_queue};
+
+    $self->{_queue} = {
+        in_queue            => 0,
+        snmp_credentials    => $self->_getValidCredentials(),
+        ranges              => [],
+        size                => 0,
+        done                => 0,
+    };
 
     my @ranges = ();
 
@@ -54,7 +169,15 @@ sub ranges {
     return @ranges;
 }
 
-sub getValidCredentials {
+sub snmp_credentials {
+    my ($self) = @_;
+
+    return unless $self->{_queue};
+
+    return $self->{_queue}->{snmp_credentials};
+}
+
+sub _getValidCredentials {
     my ($self) = @_;
 
     my @credentials;
