dace369856a6ad50e707a376a31f03302bb81726
Guillaume Bougard <gbougard@teclib.com>
2022-11-16 18:42:45 +0100

    feat: Bump netdiscovery & netinventory tasks version to 6.0

--- lib/GLPI/Agent/Task/NetDiscovery/Version.pm
+++ lib/GLPI/Agent/Task/NetDiscovery/Version.pm
@@ -3,6 +3,6 @@ package GLPI::Agent::Task::NetDiscovery::Version;
 use strict;
 use warnings;
 
-use constant VERSION => "5.1";
+use constant VERSION => "6.0";
 
 1;
--- lib/GLPI/Agent/Task/NetInventory/Version.pm
+++ lib/GLPI/Agent/Task/NetInventory/Version.pm
@@ -3,6 +3,6 @@ package GLPI::Agent::Task::NetInventory::Version;
 use strict;
 use warnings;
 
-use constant VERSION => "5.1";
+use constant VERSION => "6.0";
 
 1;
