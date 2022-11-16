03f2bedc24731b001651637fae17adde2c919e22
Guillaume Bougard <gbougard@teclib.com>
2022-11-16 18:40:48 +0100

    refacto: Use Parallel::ForkManager for netinventory task

--- lib/GLPI/Agent/Task/NetInventory/Job.pm
+++ lib/GLPI/Agent/Task/NetInventory/Job.pm
@@ -6,16 +6,21 @@ use warnings;
 use English qw(-no_match_vars);
 
 use GLPI::Agent::Logger;
+use GLPI::Agent::Tools;
 
 sub new {
     my ($class, %params) = @_;
+
+    my $devices = ref($params{devices}) eq 'ARRAY' ? $params{devices} : [];
 
     my $self = {
         logger          => $params{logger} || GLPI::Agent::Logger->new(),
         _params         => $params{params},
         _credentials    => $params{credentials},
-        _devices        => $params{devices},
+        _devices        => $devices,
+        _count          => scalar(@{$devices})
     };
+
     bless $self, $class;
 }
 
@@ -36,7 +41,7 @@ sub max_threads {
 
 sub count {
     my ($self) = @_;
-    return scalar(@{$self->{_devices}});
+    return $self->{_count};
 }
 
 sub devices {
@@ -45,19 +50,79 @@ sub devices {
     return @{$self->{_devices}};
 }
 
-sub credentials {
+sub skip_start_stop {
     my ($self) = @_;
-
-    unless (defined($self->{_credentials})) {
-        $self->{logger}->warning("No SNMP credential defined for this job");
-        return {};
-    }
-
-    $self->{logger}->warning("No SNMP credential provided for this job")
-        unless @{$self->{_credentials}};
-
-    # index credentials by their ID
-    return { map { $_->{ID} => $_ } @{$self->{_credentials}} };
+    return $self->{_params}->{NO_START_STOP} // 0;
 }
 
+sub credential {
+    my ($self, $id) = @_;
+
+    my $credential;
+
+    if (!defined($self->{_credentials})) {
+        $self->{logger}->warning("No SNMP credential defined for this job");
+    } elsif (!@{$self->{_credentials}}) {
+        $self->{logger}->warning("No SNMP credential provided for this job")
+    } else {
+        ($credential) = first { $_->{ID} == $id } @{$self->{_credentials}}
+            or $self->{logger}->warning("No SNMP credential with $id ID provided");
+    }
+
+    return $credential;
+}
+
+sub updateQueue {
+    my ($self, $devices) = @_;
+
+    return unless @{$devices};
+
+    unless ($self->{_queue}) {
+        $self->{_queue} = {
+            in_queue        => 0,
+            todo            => []
+        };
+    }
+
+    push @{$self->{_queue}->{todo}}, @{$devices};
+}
+
+sub done {
+    my ($self) = @_;
+
+    return 0 unless $self->{_queue};
+
+    $self->{_queue}->{in_queue} --;
+
+    return $self->{_queue}->{in_queue} || @{$self->{_queue}->{todo}} ? 0 : 1;
+}
+
+sub no_more {
+    my ($self) = @_;
+
+    return 0 unless $self->{_queue};
+
+    return @{$self->{_queue}->{todo}} ? 0 : 1 ;
+}
+
+sub max_in_queue {
+    my ($self) = @_;
+
+    return 0 unless $self->{_queue};
+
+    return $self->{_queue}->{in_queue} >= $self->max_threads() ? 1 : 0;
+}
+
+sub nextdevice {
+    my ($self) = @_;
+
+    return unless $self->{_queue};
+
+    my $device = shift @{$self->{_queue}->{todo}}
+        or return;
+
+    $self->{_queue}->{in_queue}++;
+
+    return $device;
+}
 1;
