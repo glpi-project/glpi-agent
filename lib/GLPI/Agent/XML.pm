fdf6e203523953dbecb3bd6bc2c9efdde409cad6
Guillaume Bougard <gbougard@teclib.com>
2022-12-01 17:30:07 +0100

    fix: Update GLPI::Agent::XML class to make calls in a dedicated thread on win32

--- lib/GLPI/Agent/XML.pm
+++ lib/GLPI/Agent/XML.pm
@@ -4,15 +4,33 @@ use strict;
 use warnings;
 
 use UNIVERSAL::require;
+
+use English qw(-no_match_vars);
 use Encode qw(encode decode);
 
 use GLPI::Agent::Tools;
+
+# We need to use a dedicated worker thread to support XML::LibXML on win32 as
+# libxml2 DLL is not fully threads-safe if few contexts
+my $need_dedicated_thread = $OSNAME eq "MSWin32" ? 1 : 0;
+if ($need_dedicated_thread) {
+    GLPI::Agent::Tools::Win32->require() or die $@;
+    GLPI::Agent::Tools::Win32::start_Win32_OLE_Worker();
+}
 
 sub new {
     my ($class, %params) = @_;
 
     my $self = {};
     bless $self, $class;
+
+    if ($need_dedicated_thread && !$params{threaded}) {
+        $self->{_id} = _GLPI_XML_win32_thread_binding(
+            api     => "new",
+            args    => [ %params, threaded => 1 ]
+        );
+        return $self;
+    }
 
     $self->string($params{string});
     $self->file($params{file}) unless $self->has_xml();
@@ -58,6 +76,15 @@ sub _xml {
 sub empty {
     my ($self) = @_;
 
+    if ($need_dedicated_thread && $self->{_id}) {
+        _GLPI_XML_win32_thread_binding(
+            _id  => $self->{_id},
+            api  => "empty",
+            args => []
+        );
+        return $self;
+    }
+
     delete $self->{_xml};
 
     return $self;
@@ -65,6 +92,14 @@ sub empty {
 
 sub has_xml {
     my ($self) = @_;
+
+    if ($need_dedicated_thread && $self->{_id}) {
+        return _GLPI_XML_win32_thread_binding(
+            _id  => $self->{_id},
+            api  => "has_xml",
+            args => []
+        );
+    }
 
     my $xml = $self->_xml;
 
@@ -75,6 +110,14 @@ sub string {
     my ($self, $string) = @_;
 
     return $self unless defined($string) && length($string);
+
+    if ($need_dedicated_thread && $self->{_id}) {
+        return _GLPI_XML_win32_thread_binding(
+            _id  => $self->{_id},
+            api  => "string",
+            args => [ $string ]
+        );
+    }
 
     $self->_init_libxml() unless $self->{_parser};
 
@@ -87,6 +130,14 @@ sub file {
     my ($self, $file) = @_;
 
     return $self unless defined($file) && -e $file;
+
+    if ($need_dedicated_thread && $self->{_id}) {
+        return _GLPI_XML_win32_thread_binding(
+            _id  => $self->{_id},
+            api  => "file",
+            args => [ $file ]
+        );
+    }
 
     $self->_init_libxml() unless $self->{_parser};
 
@@ -179,6 +230,14 @@ sub _build_xml {
 sub write {
     my ($self, $hash) = @_;
 
+    if ($need_dedicated_thread && $self->{_id}) {
+        return _GLPI_XML_win32_thread_binding(
+            _id  => $self->{_id},
+            api  => "write",
+            args => [ $hash ]
+        );
+    }
+
     if ($hash) {
         $self->empty->_build_xml($hash)
             or return;
@@ -213,6 +272,14 @@ sub writefile {
 # Recursive API to dump XML::LibXML objects as a hash tree more like XML::TreePP does
 sub dump_as_hash {
     my ($self, $node) = @_;
+
+    if ($need_dedicated_thread && $self->{_id}) {
+        return _GLPI_XML_win32_thread_binding(
+            _id  => $self->{_id},
+            api  => "dump_as_hash",
+            args => [ $node ]
+        );
+    }
 
     unless ($node) {
         my $xml = $self->_xml()
@@ -283,6 +350,52 @@ sub dump_as_hash {
     }
 
     return $ret;
+}
+
+# On win32, we want to cache XML objects in a dedicated thread
+my %XMLs;
+my $xmlid = 0;
+sub _GLPI_XML_win32_binded_thread {
+    my (%infos) = @_;
+
+    my $api = $infos{api};
+    my $id  = $infos{_id};
+
+    if (defined($id)) {
+        if ($infos{destroy}) {
+           delete $XMLs{$id};
+            return;
+        } else {
+            # API call on cached object
+            return $XMLs{$id}->$api(@{$infos{args}});
+        }
+    }
+
+    # Keep a cache id as simple integer
+    $xmlid = ++$xmlid % 4294967296 ;
+    while (exists($XMLs{$xmlid})) { $xmlid++ };
+
+    $XMLs{$xmlid} = GLPI::Agent::XML->new(@{$infos{args}});
+    return $xmlid;
+}
+
+sub _GLPI_XML_win32_thread_binding {
+    my (%params) = @_;
+
+    return GLPI::Agent::Tools::Win32::call_not_thread_safe_api_on_win32({
+        module => 'GLPI::Agent::XML',
+        funct  => '_GLPI_XML_win32_binded_thread',
+        args   => \@_
+    });
+}
+
+sub DESTROY {
+    local($., $@, $!, $^E, $?);
+    my ($self) = @_;
+    $self->{_id} and _GLPI_XML_win32_thread_binding(
+        _id     => $self->{_id},
+        destroy => 1
+    );
 }
 
 1;
