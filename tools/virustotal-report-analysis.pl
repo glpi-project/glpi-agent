107745babc7d76112b3033cc3e5cfdfb449c0d1d
Guillaume Bougard <gbougard@teclib.com>
2023-05-10 18:26:53 +0200

    build: Updated virustotal report analysis

--- tools/virustotal-report-analysis.pl
+++ tools/virustotal-report-analysis.pl
@@ -7,7 +7,6 @@ use JSON;
 use LWP::UserAgent;
 use Digest::SHA qw($errmsg);
 use English qw(-no_match_vars);
-use UNIVERSAL::require;
 
 unless ($ENV{VT_API_KEY}) {
     print STDERR "Set VT_API_KEY environment variable to your VirusTotal API Key if you want to check VirtusTotal reports.\n";
@@ -23,6 +22,8 @@ while (@ARGV) {
         push @sha256, shift @ARGV;
     } elsif ($arg =~ /^--path$/) {
         $path = shift @ARGV;
+        chdir $path
+            or die "Can't change directory to $path: $!\n";
     } elsif (-e $arg) {
         my $digest = eval { Digest::SHA->new(256)->addfile($arg, "b") };
         if ($@) {
@@ -48,6 +49,7 @@ my @failed;
 
 foreach my $sha256 (@sha256) {
     my $url = "$baseurl/$sha256";
+    print "debug: requesting $url...\n";
     my $response = $ua->get( $url, 'x-apikey' => $ENV{VT_API_KEY});
     die "$url error: ".$response->status_line."\n"
        unless $response->is_success;
@@ -57,21 +59,15 @@ foreach my $sha256 (@sha256) {
     my $json = JSON->new->allow_nonref->decode($content)
         or die "$url error: No json decoded from '$content'\n";
     die "$url error: No expected data in '".JSON->new->allow_nonref->pretty->encode($json)."'\n"
-     unless $json->{data} && $json->{data}->{attributes} && $json->{data}->{attributes}->{last_analysis_stats};
+        unless $json->{data} && $json->{data}->{attributes} && $json->{data}->{attributes}->{last_analysis_stats};
     my $stat = $json->{data}->{attributes}->{last_analysis_stats};
     push @failed, $sha256 if $stat->{suspicious} || $stat->{malicious};
-    if ($path) {
-        my $handle;
-        if ($OSNAME eq 'MSWin32' && Win32::Unicode::File->require()) {
-            $handle = Win32::Unicode::File->new('w', "$path\\$sha256.json");
-        } else {
-            open $handle, ">", "$path/$sha256.json";
-        }
-        if ($handle) {
-            print $handle $content;
-            close($handle);
-        }
-    }
+
+    my $handle;
+    open $handle, ">", "$sha256.json"
+        or die "Can't write $sha256.json to $path: $!\n";
+    print $handle $content;
+    close($handle);
 }
 
 if (@failed) {
