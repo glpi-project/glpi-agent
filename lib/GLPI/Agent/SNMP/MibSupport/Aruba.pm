b78517ce8cb897c3712bbb40547a22a34a38995f
Guillaume Bougard <gbougard@teclib.com>
2023-08-07 11:09:09 +0200

    feat: Enhanced Aruba IAP models support

--- lib/GLPI/Agent/SNMP/MibSupport/Aruba.pm
+++ lib/GLPI/Agent/SNMP/MibSupport/Aruba.pm
@@ -8,7 +8,18 @@ use parent 'GLPI::Agent::SNMP::MibSupportTemplate';
 use GLPI::Agent::Tools;
 use GLPI::Agent::Tools::SNMP;
 
+# See Q-BRIDGE-MIB
+use constant    dot1qTpFdbStatus => '.1.3.6.1.2.1.17.7.1.2.2.1.3';
+
+# See ARUBA-MIB
 use constant    aruba   => '.1.3.6.1.4.1.14823' ;
+
+# See AI-AP-MIB
+use constant    aiMIB   => aruba . '.2.3.3.1' ;
+
+use constant    aiVirtualControllerVersion  => aiMIB . '.1.4.0';
+use constant    aiAPSerialNum               => aiMIB . '.2.1.1.4';
+use constant    aiAPModelName               => aiMIB . '.2.1.1.6';
 
 our $mibSupport = [
     {
@@ -17,8 +28,46 @@ our $mibSupport = [
     }
 ];
 
+sub getFirmware {
+    my ($self) = @_;
+
+    return getCanonicalString($self->get(aiVirtualControllerVersion));
+}
+
+sub _this {
+    my ($self) = @_;
+
+    unless ($self->{_this}) {
+        # Find reference to our device
+        my $dot1qTpFdbStatus = $self->walk(dot1qTpFdbStatus);
+        if ($dot1qTpFdbStatus) {
+            my ($subkey) = first {
+                $dot1qTpFdbStatus->{$_} eq '4' } keys(%{$dot1qTpFdbStatus});
+            my ($extracted) = $subkey =~ /^\d+\.(.*)$/;
+            $self->{_this} = $extracted if $extracted;
+        }
+    }
+
+    return $self->{_this};
+}
+
+sub getSerial {
+    my ($self) = @_;
+
+    my $this = $self->_this
+        or return;
+
+    return getCanonicalString($self->get(aiAPSerialNum.'.'.$this));
+}
+
 sub getModel {
     my ($self) = @_;
+
+    my $this = $self->_this;
+    if ($this) {
+        my $model = getCanonicalString($self->get(aiAPModelName.'.'.$this));
+        return "AP $model" if $model;
+    }
 
     my $device = $self->device
         or return;
