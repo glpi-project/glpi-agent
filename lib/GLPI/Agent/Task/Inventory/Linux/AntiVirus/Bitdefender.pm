fec5235005653d03440abd2c859cd275b8098e38
Oscar B <oscar@ticgal.com>
2023-09-03 22:27:49 +0200

    Add missing path as suggested
    Signed-off-by: Oscar B. <oscar@ticgal.com>

--- lib/GLPI/Agent/Task/Inventory/Linux/AntiVirus/Bitdefender.pm
+++ lib/GLPI/Agent/Task/Inventory/Linux/AntiVirus/Bitdefender.pm
@@ -10,7 +10,7 @@ use UNIVERSAL::require;
 use GLPI::Agent::Tools;
 
 sub isEnabled {
-    return canRun('bduitool');
+    return canRun('/opt/bitdefender-security-tools/bin/bduitool');
 }
 
 sub doInventory {
