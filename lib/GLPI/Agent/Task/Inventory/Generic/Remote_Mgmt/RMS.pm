8eec38eb0f2b2bda4387b4fd9ac7f9e881f11e6d
yes1n <yes1n@mail.ru>
2023-09-25 22:05:33 +0300

    RMS_MGMT
    https://github.com/glpi-project/glpi-agent/discussions/495

---
+++ lib/GLPI/Agent/Task/Inventory/Generic/Remote_Mgmt/RMS.pm
@@ -0,0 +1,90 @@
+package GLPI::Agent::Task::Inventory::Generic::Remote_Mgmt::RMS;
+
+use strict;
+use warnings;
+
+use parent 'GLPI::Agent::Task::Inventory::Module';
+
+use English qw(-no_match_vars);
+
+use GLPI::Agent::Tools;
+
+sub isEnabled {
+    my (%params) = @_;
+
+    if (OSNAME eq 'MSWin32') {
+
+        GLPI::Agent::Tools::Win32->use();
+
+        my $key = getRegistryKey(
+            path => 'HKEY_LOCAL_MACHINE/SOFTWARE/Usoris/Remote Utilities Host/Host/Parameters',
+            # Important for remote inventory optimization
+            required    => [ qw/InternetId/ ],
+            maxdepth    => 1,
+            logger => $params{logger}
+        );
+        if (!$key && is64bit()) {
+            $key = getRegistryKey(
+                path => 'HKEY_LOCAL_MACHINE/SOFTWARE/Usoris/Remote Utilities Host/Host/Parameters',
+                # Important for remote inventory optimization
+                required    => [ qw/InternetId/ ],
+                maxdepth    => 1,
+                logger => $params{logger}
+            );
+        }
+
+        return $key && (keys %$key);
+    }
+
+    return canRun('rms');
+}
+
+sub doInventory {
+    my (%params) = @_;
+
+    my $inventory = $params{inventory};
+    my $logger    = $params{logger};
+
+    my $InternetID = _getID(
+        osname  => OSNAME,
+        logger  => $logger
+    );
+    if (defined($InternetID)) {
+        $logger->debug('Found InternetID : ' . $InternetID) if ($logger);
+
+        $inventory->addEntry(
+            section => 'REMOTE_MGMT',
+            entry   => {
+                ID   => $InternetID,
+                TYPE => 'rms'
+            }
+        );
+    } else {
+        $logger->debug('InternetID not found') if ($logger);
+    }
+}
+
+sub _getID {
+    my (%params) = @_;
+    my $osname = delete $params{osname} // '';
+    return _getID_MSWin32() if $osname eq "MSWin32";
+}
+
+sub _getID_MSWin32 {
+
+    GLPI::Agent::Tools::Win32->use();
+
+    my $clientid = getRegistryValue(
+        path => 'HKEY_LOCAL_MACHINE/SOFTWARE/Usoris/Remote Utilities Host/Host/Parameters/InternetId',
+    );
+
+    $clientid = hex2dec($clientid);
+
+    $clientid = substr($clientid, 0, index($clientid, '</internet_id'));
+    $clientid = substr($clientid, index($clientid, '<internet_id>')+13);
+
+    return $clientid;
+
+}
+
+1;
---
+++ t/tasks/inventory/generic/remote_mgmt/rms.t
@@ -0,0 +1,48 @@
+#!/usr/bin/perl
+
+use strict;
+use warnings;
+
+use lib 't/lib';
+
+use English qw(-no_match_vars);
+use Test::Deep qw(cmp_deeply);
+use Test::More;
+use Test::MockModule;
+use Test::NoWarnings;
+
+use GLPI::Test::Utils;
+
+use GLPI::Agent::Task::Inventory::Generic::Remote_Mgmt::RMS;
+
+BEGIN {
+    # use mock modules for non-available ones
+    push @INC, 't/lib/fake/windows' if $OSNAME ne 'MSWin32';
+}
+
+my $module = Test::MockModule->new(
+    'GLPI::Agent::Tools::Win32'
+);
+
+foreach my $test () {
+    $module->mock(
+        '_getRegistryKey',
+        _mockGetRegistryKey($test)
+    );
+    my $internetID = GLPI::Agent::Task::Inventory::Generic::Remote_Mgmt::RMS::_getID(osname => "MSWin32");
+    is($internetID, "RMS win32 getID - $test");
+}
+
+# Adapted from GLPI::Test::Utils mockGetRegistryKey()
+sub _mockGetRegistryKey {
+    my ($test) = @_;
+
+    return sub {
+        my (%params) = @_;
+
+        # We can mock getRegistryKey or better _getRegistryKey to cover getRegistryValue
+        my $path = $params{path} || $params{keyName};
+        my $file = "resources/generic/rms/rms.reg";
+        return loadRegistryDump($file);
+    };
+}
