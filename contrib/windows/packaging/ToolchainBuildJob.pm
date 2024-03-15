bfe69f217a4750e37d4da23ac0661b57a4b8b396
Guillaume Bougard <gbougard@teclib.com>
2024-03-15 15:04:19 +0100

    build: Update libxml2 to 2.12.6 in windows packaging

--- contrib/windows/packaging/ToolchainBuildJob.pm
+++ contrib/windows/packaging/ToolchainBuildJob.pm
@@ -192,7 +192,7 @@ sub toolchain_build_steps {
             {
                 plugin  => 'Perl::Dist::Strawberry::Step::BuildLibrary',
                 name    => 'libxml2',
-                version => '2.12.5',
+                version => '2.12.6',
                 url     => 'https://download.gnome.org/sources/<name>/2.12/<name>-<version>.tar.xz',
                 skip_if_file    => 'bin/libxml2-2__.dll',
                 skip_test       => 1,
