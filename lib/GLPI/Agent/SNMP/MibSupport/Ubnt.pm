524b425a855c3a2f15060d30d672986adf1a060c
Eduardo Mozart de Oliveira <2974895+eduardomozart@users.noreply.github.com>
2024-04-22 11:09:52 -0300

    Improve serial and SSID detection
    Show SSID names instead of radio ifname on GLPI UI and fix Serial getting on UniFi AP series.

--- lib/GLPI/Agent/SNMP/MibSupport/Ubnt.pm
+++ lib/GLPI/Agent/SNMP/MibSupport/Ubnt.pm
@@ -10,20 +10,40 @@ use GLPI::Agent::Tools::SNMP;
 
 # See UBNT-MIB
 
-use constant ubnt               => '.1.3.6.1.4.1.41112';
-use constant ubntWlStatApMac    => ubnt . '.1.4.5.1.4.1';
+use constant ubnt              => '.1.3.6.1.4.1.41112';
+use constant ubntWlStatApMac   => ubnt . '.1.4.5.1.4.1';
+
+# See UBNT-UniFi-MIB
+
+use constant unifiVapEssid        => ubnt . '.1.6.1.2.1.6';
+use constant unifiVapName         => ubnt . '.1.6.1.2.1.7';
+use constant unifiApSystemVersion => ubnt . '.1.6.3.6.0';
+use constant unifiApSystemModel	  => ubnt . '.1.6.3.3.0';
+
 
 our $mibSupport = [
     {
         name    => "ubnt",
-        oid     => ubnt
+        sysobjectid => getRegexpOidMatch(ubnt)
     }
 ];
+
+sub getFirmware {
+    my ($self) = @_;
+
+    return getCanonicalString($self->get(unifiApSystemVersion));
+}
+
+sub getModel {
+    my ($self) = @_;
+
+    return getCanonicalString($self->get(unifiApSystemModel));
+}
 
 sub getSerial {
     my ($self) = @_;
 
-    my $serial = getCanonicalMacAddress($self->get(ubntWlStatApMac));
+    my $serial = $self->getMacAddress;
     $serial =~ s/://g;
 
     return $serial;
@@ -31,8 +51,49 @@ sub getSerial {
 
 sub getMacAddress {
     my ($self) = @_;
+	
+	my $device = $self->device
+        or return;
+	
+	my $serial = getCanonicalMacAddress($self->get(ubntWlStatApMac));
+	$serial = $device->{MAC}
+				if not defined ($serial);
 
-    return $self->get(ubntWlStatApMac);
+    return $serial;
+}
+
+	
+
+sub run {
+	my ($self) = @_;
+	
+	my $device = $self->device
+        or return;
+		
+	my $ports = $device->{PORTS}->{PORT};
+	
+	my $unifiVapEssidValues = $self->walk(unifiVapEssid) || {};
+	my $unifiVapNameValues = $self->walk(unifiVapName) || {};
+	
+	foreach my $port (keys(%$ports)) {
+		my $ifdescr = $device->{PORTS}->{PORT}->{$port}->{IFDESCR};
+		next unless ($ifdescr !~ s/^(?!ra)//g);
+		
+		foreach my $index (keys(%$unifiVapNameValues)) {
+			if ($ifdescr eq $unifiVapNameValues->{$index}) {
+				$device->{PORTS}->{PORT}->{$port}->{IFALIAS} = $ifdescr;
+				$device->{PORTS}->{PORT}->{$port}->{IFNAME} = getCanonicalString($unifiVapEssidValues->{$index});
+				
+				if($ifdescr =~ s/^ra(\d+)$//g) {
+					$device->{PORTS}->{PORT}->{$port}->{IFNAME} .= " (2.4GHz)";
+				} elsif($ifdescr =~ s/^rai(\d+)$//g) {
+					$device->{PORTS}->{PORT}->{$port}->{IFNAME} .= " (5GHz)";
+				}
+				
+				last;
+			}
+		}
+	}
 }
 
 1;
