ff8a4e9142d85847ac484c95e8afb9ed165f9708
Eduardo Mozart de Oliveira <2974895+eduardomozart@users.noreply.github.com>
2024-04-22 11:17:34 -0300

    Update Device.pm

--- lib/GLPI/Agent/SNMP/Device.pm
+++ lib/GLPI/Agent/SNMP/Device.pm
@@ -8,7 +8,6 @@ use GLPI::Agent::Tools::SNMP;
 use GLPI::Agent::Tools::Network;
 use GLPI::Agent::SNMP::MibSupport;
 use GLPI::Agent::SNMP::Device::Components;
-use Encode;
 
 # Supported infos are specified here:
 # http://fusioninventory.org/documentation/dev/spec/protocol/netdiscovery.html
@@ -479,9 +478,6 @@ sub setManufacturer {
 sub setBaseInfos {
     my ($self) = @_;
     $self->_set_from_oid_list($base_variables, $self);
-
-    $self->{LOCATION} = Encode::decode('utf8', $self->{LOCATION})
-				if defined($self->{LOCATION});
 
     # Filter out LOCATION & CONTACT from unwanted values
     if ($self->{LOCATION} && $self->{LOCATION} =~ /edit \/etc.*snmp.*\.conf/) {
