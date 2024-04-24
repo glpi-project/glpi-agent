b270a37d4cdc44a3ad057f73c25bb909039df5aa
Eduardo Mozart de Oliveira <2974895+eduardomozart@users.noreply.github.com>
2024-04-24 07:47:49 -0300

    Update lib/GLPI/Agent/SNMP/MibSupport/Ubnt.pm
    Co-authored-by: Guillaume Bougard <gbougard@teclib.com>

--- lib/GLPI/Agent/SNMP/MibSupport/Ubnt.pm
+++ lib/GLPI/Agent/SNMP/MibSupport/Ubnt.pm
@@ -95,13 +95,17 @@ sub run {
                 # Defines the port alias with the name of the radio (e.g. raX)
                 $device->{PORTS}->{PORT}->{$port}->{IFALIAS} = $ifdescr;
                 # Replaces the radio port name with its respective <SSID>
-                $device->{PORTS}->{PORT}->{$port}->{IFNAME} = getCanonicalString($unifiVapEssidValues->{$index});
+                my $ifname = getCanonicalString($unifiVapEssidValues->{$index});
 
-                # raX and raiX are the network interfaces for the 2.4GHz and 5GHz radios respectively
-                if ($ifdescr =~ m/^ra\d+$/) {
-                    $device->{PORTS}->{PORT}->{$port}->{IFNAME} .= " (2.4GHz)";
-                } elsif ($ifdescr =~ m/^rai\d+$/) {
-                    $device->{PORTS}->{PORT}->{$port}->{IFNAME} .= " (5GHz)";
+                unless (empty($ifname)) {
+                    # raX and raiX are the network interfaces for the 2.4GHz and 5GHz radios respectively
+                    if ($ifdescr =~ m/^ra\d+$/) {
+                        $ifname .= " (2.4GHz)";
+                    } elsif ($ifdescr =~ m/^rai\d+$/) {
+                        $ifname .= " (5GHz)";
+                    }
+
+                    $device->{PORTS}->{PORT}->{$port}->{IFNAME} = $ifname;
                 }
 
                 last;
