676721d4419d4be21392f695a083a675143329d3
Guillaume Bougard <gbougard@teclib.com>
2024-04-26 15:12:29 +0200

    fix: Fix running from sources from using symbolic links

--- lib/setup.pm
+++ lib/setup.pm
@@ -6,7 +6,7 @@ use warnings;
 use parent qw(Exporter);
 
 use File::Spec;
-use Cwd qw(abs_path);
+use File::Basename qw(dirname);
 
 our @EXPORT = ('%setup');
 
@@ -16,19 +16,18 @@ our %setup = (
     vardir  => './var',
 );
 
-# Compute directly libdir from this setup file as it should be installed
-# in expected directory
+# Compute directly libdir from this setup file as it should be installed in expected directory
 eval {
-    $setup{libdir} = abs_path(File::Spec->rel2abs('..', __FILE__))
-        unless ($setup{libdir} && File::Spec->file_name_is_absolute($setup{libdir}));
+    $setup{libdir} = File::Spec->rel2abs(dirname(__FILE__))
+        unless $setup{libdir} && File::Spec->file_name_is_absolute($setup{libdir});
 
     # If run from sources, we can try to rebase setup keys to absolute folders related to libdir
     if (File::Spec->file_name_is_absolute($setup{libdir})) {
         foreach my $key (qw(datadir vardir)) {
             # Anyway don't update if target still absolute
-            next if ($setup{$key} && File::Spec->file_name_is_absolute($setup{$key}));
+            next if $setup{$key} && File::Spec->file_name_is_absolute($setup{$key});
 
-            my $folder = abs_path(File::Spec->rel2abs('../'.$setup{$key}, $setup{libdir}));
+            my $folder = File::Spec->rel2abs($setup{$key}, dirname($setup{libdir}));
             $setup{$key} = $folder if $folder && -d $folder;
         }
     }
