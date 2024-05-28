e0511fd6bfb0c2ded4d9a053ee98acc9f89d7830
Guillaume Bougard <gbougard@teclib.com>
2024-05-28 14:53:55 +0200

    feat: update for 1.10 development

--- lib/GLPI/Agent/Version.pm
+++ lib/GLPI/Agent/Version.pm
@@ -3,7 +3,7 @@ package GLPI::Agent::Version;
 use strict;
 use warnings;
 
-our $VERSION = "1.9";
+our $VERSION = "1.10-dev";
 our $PROVIDER = "GLPI";
 our $COMMENTS = [];
 
@@ -31,5 +31,5 @@ agent issue is reported.
 One very useful information should be first defined like in that example:
 
 our $COMMENTS = [
-    "Based on GLPI Agent 1.9"
+    "Based on GLPI Agent 1.10-dev"
 ];
