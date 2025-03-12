6bb1b571350027a57e6c3c4c83c66b9f20893bca
Jo-NMS <jonathan.arvier@nameshield.net>
2025-03-12 09:32:57 +0100

    Update lib/GLPI/Agent/Task/Inventory/Virtualization/Lxc.pm

--- lib/GLPI/Agent/Task/Inventory/Virtualization/Lxc.pm
+++ lib/GLPI/Agent/Task/Inventory/Virtualization/Lxc.pm
@@ -102,17 +102,10 @@ sub  _getVirtualMachine {
                 if $val =~ $mac_address_pattern;
         }
 
-    	if ($key eq 'lxc.cgroup.memory.limit_in_bytes' || $key eq 'lxc.cgroup2.memory.max') {
-    	    if ($val =~ /[KMGTP]$/i) {
-    		$val .= "b";
-    		$container->{MEMORY} = getCanonicalSize($val, 1024);
-    	    } elsif ($val =~ /^\d+$/) {
-    		# Plain numeric value in bytes, convert to MB
-    		$container->{MEMORY} = int($val / (1024 * 1024));
-    	    } else {
-    		$container->{MEMORY} = getCanonicalSize($val, 1024);
-    	    }
-    	}	
+        if ($key eq 'lxc.cgroup.memory.limit_in_bytes' || $key eq 'lxc.cgroup2.memory.max') {
+            $val .= $val =~ /[KMGTP]$/i ? "b" : $val =~ /^\d+$/ ? "bytes" : "";
+            $container->{MEMORY} = getCanonicalSize($val, 1024);
+        }
 
         # Update container name in Proxmox environment
         if ($proxmox && ($key eq 'lxc.uts.name' || $key eq 'lxc.utsname')) {
