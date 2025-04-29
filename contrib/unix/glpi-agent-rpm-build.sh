8c04c2e1000f68b4005f92e9ace116312ecace0d
Guillaume Bougard <gbougard@teclib.com>
2025-04-29 13:07:41 +0200

    build: Fix rpm packaging versioning for v1.14-2 RPM

--- contrib/unix/glpi-agent-rpm-build.sh
+++ contrib/unix/glpi-agent-rpm-build.sh
@@ -39,7 +39,8 @@ if [ ! -e lib -a ! -e tools/make-release.sh ]; then
     exit 1
 fi
 
-VER="1.14-2"
+VER=
+REV="2"
 if [ -n "${GITHUB_REF%refs/tags/*}" -o -z "$VER" ]; then
     VER=$(perl -Ilib -MGLPI::Agent::Version -e '$v = $GLPI::Agent::Version::VERSION; $v =~ s/-.*//; print $v')
 fi
