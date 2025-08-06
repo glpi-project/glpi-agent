93cc0caf8c8a187ac5c57e2f90bced6c1e0722d8
Guillaume Bougard <gbougard@teclib.com>
2025-08-06 09:27:49 +0200

    build: Use OpenSSL 3.5.2 for windows and macosx packaging

--- contrib/windows/packaging/ToolchainBuildJob.pm
+++ contrib/windows/packaging/ToolchainBuildJob.pm
@@ -179,7 +179,7 @@ sub toolchain_build_steps {
             {
                 plugin  => 'Perl::Dist::Strawberry::Step::BuildLibrary',
                 name    => 'openssl',
-                version => '3.5.1',
+                version => '3.5.2',
                 url     => 'https://github.com/openssl/openssl/releases/download/<name>-<version>/<name>-<version>.tar.gz',
                 skip_if_file    => 'bin/openssl.exe',
                 skip_test       => 1,
