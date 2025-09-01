e9d328ff6caf53748ecbef77e6f615c15b314c43
Guillaume Bougard <gbougard@teclib.com>
2025-09-01 18:42:18 +0200

    build: Update msys2-base for windows build toolchain

--- contrib/windows/packaging/ToolchainBuildJob.pm
+++ contrib/windows/packaging/ToolchainBuildJob.pm
@@ -39,8 +39,8 @@ sub toolchain_build_steps {
             {
                 plugin          => 'Perl::Dist::Strawberry::Step::Msys2',
                 name            => 'msys2-base',
-                version         => '20250622',
-                folder          => '2025-06-22',
+                version         => '20250830',
+                folder          => '2025-08-30',
                 url             => 'https://github.com/msys2/msys2-installer/releases/download/<folder>/<name>-x86_64-<version>.tar.xz',
                 dest            => 'msys64',
             },
