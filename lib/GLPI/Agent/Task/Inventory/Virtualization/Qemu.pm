9f8e442f5fd70399d6ca54a6d72cd108ed356c8b
Megachip <Megachip@users.noreply.github.com>
2025-09-15 21:23:34 +0200

    Adding qemu CPU number detection (#1010)
    Co-authored-by: Guillaume Bougard <gbougard@teclib.com>

--- lib/GLPI/Agent/Task/Inventory/Virtualization/Qemu.pm
+++ lib/GLPI/Agent/Task/Inventory/Virtualization/Qemu.pm
@@ -47,6 +47,11 @@ sub _parseProcessList {
             $values->{uuid} = $1;
         } elsif ($option =~ m/^enable-kvm/) {
             $values->{vmtype} = "kvm";
+        } elsif ($option =~ m/^smp (\S+)$/) {
+            my ($cpus) = grep { /^(?:cpus=)?\d+$/ } split(/,/, $1);
+            if ($cpus && $cpus =~ /(\d+)$/) {
+                $values->{vcpu} = int($1);
+            }
         }
 
         if ($option =~ /smbios/) {
@@ -94,7 +99,7 @@ sub doInventory {
             entry => {
                 NAME      => $values->{name},
                 UUID      => $values->{uuid},
-                VCPU      => 1,
+                VCPU      => $values->{vcpu} || 1,
                 MEMORY    => $values->{mem},
                 STATUS    => STATUS_RUNNING,
                 SUBSYSTEM => $values->{vmtype},
