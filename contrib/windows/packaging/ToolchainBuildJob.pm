6edeafaf8cc3acc9bd3be71b4e5f79960fc92d54
Guillaume Bougard <gbougard@teclib.com>
2025-09-16 12:35:45 +0200

    build: Use libxml2 v2.15.0 for windows build

--- contrib/windows/packaging/ToolchainBuildJob.pm
+++ contrib/windows/packaging/ToolchainBuildJob.pm
@@ -222,8 +222,8 @@ sub toolchain_build_steps {
             {
                 plugin  => 'Perl::Dist::Strawberry::Step::BuildLibrary',
                 name    => 'libxml2',
-                version => '2.14.6',
-                url     => 'https://download.gnome.org/sources/<name>/2.14/<name>-<version>.tar.xz',
+                version => '2.15.0',
+                url     => 'https://download.gnome.org/sources/<name>/2.15/<name>-<version>.tar.xz',
                 skip_if_file    => 'bin/libxml2-16__.dll',
                 skip_test       => 1,
                 configure_help  => 1,
