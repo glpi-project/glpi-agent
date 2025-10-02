cd39d2f3120dcdc20f35db17c2f5480d7f0df831
Guillaume Bougard <gbougard@teclib.com>
2025-10-02 17:34:15 +0200

    fix: Add Tiesse devices support

---
+++ lib/GLPI/Agent/SNMP/MibSupport/Tiesse.pm
@@ -0,0 +1,55 @@
+package GLPI::Agent::SNMP::MibSupport::Tiesse;
+
+use strict;
+use warnings;
+
+use parent 'GLPI::Agent::SNMP::MibSupportTemplate';
+
+use GLPI::Agent::Tools;
+use GLPI::Agent::Tools::SNMP;
+
+use constant    sysDescr    => '.1.3.6.1.2.1.1.1.0';
+
+use constant    entPhysicalDescr    => '.1.3.6.1.2.1.47.1.1.1.1.2.0';
+
+use constant    tiesse  => ".1.3.6.1.4.1.4799" ;
+
+use constant    privatePhysicalDescr  => tiesse . ".3.2.6023.0";
+
+our $mibSupport = [
+    {
+        name        => "tiesse",
+        sysobjectid => getRegexpOidMatch(tiesse)
+    }
+];
+
+sub getModel {
+    my ($self) = @_;
+
+    my $model = getCanonicalString($self->get(privatePhysicalDescr) || $self->get(entPhysicalDescr));
+
+    ($model) = $model =~ /^(?:\S+) (\S+\s\S+)/
+        if $model && $model =~ /^Tiesse/i;
+
+    return $model;
+}
+
+sub getManufacturer {
+    return "Tiesse";
+}
+
+sub getType {
+    return 'NETWORKING';
+}
+
+1;
+
+__END__
+
+=head1 NAME
+
+GLPI::Agent::SNMP::MibSupport::Htek - Inventory module for Tiesse devices
+
+=head1 DESCRIPTION
+
+The module enhances support for Tiesse devices
