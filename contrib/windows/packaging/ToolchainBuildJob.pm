df88c415f9f2a6d19e07c1ba2efbc8c46b5f918c
Guillaume Bougard <gbougard@teclib.com>
2025-10-31 13:34:29 +0100

    build: Use gcc 15.2.0posix-13.0.0-msvcrt-r3 for windows packaging

--- contrib/windows/packaging/ToolchainBuildJob.pm
+++ contrib/windows/packaging/ToolchainBuildJob.pm
@@ -6,8 +6,8 @@ use parent 'Exporter';
 use constant {
     # Toolchain setup
     TOOLCHAIN_BASE_URL  => 'https://github.com/brechtsanders/winlibs_mingw/releases/download',
-    TOOLCHAIN_VERSION   => '15.2.0posix-13.0.0-msvcrt-r1',
-    TOOLCHAIN_ARCHIVE   => 'winlibs-x86_64-posix-seh-gcc-15.2.0-mingw-w64msvcrt-13.0.0-r1.zip'
+    TOOLCHAIN_VERSION   => '15.2.0posix-13.0.0-msvcrt-r3',
+    TOOLCHAIN_ARCHIVE   => 'winlibs-x86_64-posix-seh-gcc-15.2.0-mingw-w64msvcrt-13.0.0-r3.zip'
 };
 
 sub toolchain_build_steps {
