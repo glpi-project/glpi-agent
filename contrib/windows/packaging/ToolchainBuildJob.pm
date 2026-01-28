6e6fc80d9d5040364f58f08af0e96c32adbd7e21
Guillaume Bougard <gbougard@teclib.com>
2026-01-28 11:28:53 +0100

    build: Use liblzma from xz 5.8.2 for windows build

--- contrib/windows/packaging/ToolchainBuildJob.pm
+++ contrib/windows/packaging/ToolchainBuildJob.pm
@@ -112,7 +112,7 @@ sub toolchain_build_steps {
             {
                 plugin  => 'Perl::Dist::Strawberry::Step::BuildLibrary',
                 name    => 'xz',
-                version => '5.8.1',
+                version => '5.8.2',
                 url     => 'https://github.com/tukaani-project/<name>/releases/download/v<version>/<name>-<version>.tar.gz',
                 skip_if_file    => 'bin/liblzma-5__.dll',
                 skip_test       => 1,
