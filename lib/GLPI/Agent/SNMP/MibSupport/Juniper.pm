9d54e262b340267d5b62f5e18887438dd827dd60
Guillaume Bougard <gbougard@teclib.com>
2026-02-27 14:46:56 +0100

    fix: Updated Juniper devices support fixing connections detection and stacking management

--- lib/GLPI/Agent/SNMP/MibSupport/Juniper.pm
+++ lib/GLPI/Agent/SNMP/MibSupport/Juniper.pm
@@ -19,10 +19,11 @@ use constant    jnxExMibRoot    => jnxMIBs . '.40';
 use constant    jnxExVirtualChassis => jnxExMibRoot . '.1.4';
 
 # See JUNIPER-VIRTUALCHASSIS-MIB
-use constant    jnxVirtualChassisMemberSerialnumber => jnxExVirtualChassis . '.1.1.1.2.0';
-use constant    jnxVirtualChassisMemberMacAddBase   => jnxExVirtualChassis . '.1.1.1.4.0';
-use constant    jnxVirtualChassisMemberSWVersion    => jnxExVirtualChassis . '.1.1.1.5.0';
-use constant    jnxVirtualChassisMemberModel        => jnxExVirtualChassis . '.1.1.1.8.0';
+use constant    jnxVirtualChassisMemberSerialnumber => jnxExVirtualChassis . '.1.1.1.2';
+use constant    jnxVirtualChassisMemberRole         => jnxExVirtualChassis . '.1.1.1.3';
+use constant    jnxVirtualChassisMemberMacAddBase   => jnxExVirtualChassis . '.1.1.1.4';
+use constant    jnxVirtualChassisMemberSWVersion    => jnxExVirtualChassis . '.1.1.1.5';
+use constant    jnxVirtualChassisMemberModel        => jnxExVirtualChassis . '.1.1.1.8';
 
 
 our $mibSupport = [
@@ -32,22 +33,38 @@ our $mibSupport = [
     }
 ];
 
+sub _getMasterKey {
+    my ($self) = @_;
+
+    my $device = $self->device
+        or return;
+
+    # Return cached value for this device
+    return $device->{_master} if defined($device->{_master});
+
+    my $role = $self->walk(jnxVirtualChassisMemberRole)
+        or return;
+
+    my $index = first { isInteger($role->{$_}) && $role->{$_} == 1 } keys(%{$role});
+    return $device->{_master} = ".$index";
+}
+
 sub getFirmware {
     my ($self) = @_;
 
-    return getCanonicalString($self->get(jnxVirtualChassisMemberSWVersion));
+    return getCanonicalString($self->get(jnxVirtualChassisMemberSWVersion.$self->_getMasterKey()));
 }
 
 sub getMacAddress {
     my ($self) = @_;
 
-    return getCanonicalMacAddress($self->get(jnxVirtualChassisMemberMacAddBase));
+    return getCanonicalMacAddress($self->get(jnxVirtualChassisMemberMacAddBase.$self->_getMasterKey()));
 }
 
 sub getModel {
     my ($self) = @_;
 
-    return getCanonicalString($self->get(jnxVirtualChassisMemberModel));
+    return getCanonicalString($self->get(jnxVirtualChassisMemberModel.$self->_getMasterKey()));
 }
 
 sub getSerial {
@@ -58,7 +75,7 @@ sub getSerial {
 
     return if $device->{SERIAL};
 
-    return getCanonicalString($self->get(jnxVirtualChassisMemberSerialnumber));
+    return getCanonicalString($self->get(jnxVirtualChassisMemberSerialnumber.$self->_getMasterKey()));
 }
 
 sub run {
@@ -91,11 +108,37 @@ sub run {
             my $merge = delete $ports->{$index{$physical}};
             map {
                 $port->{$_} = $merge->{$_} if $merge->{$_}
-            } qw( IFNAME IFDESCR IFTYPE IFSPEED VLAN);
+            } qw( IFNAME IFDESCR IFTYPE IFSPEED VLAN CONNECTIONS );
             map {
                 $port->{$_} = 0 unless $port->{$_};
                 $port->{$_} += $merge->{$_} if $merge->{$_};
             } qw( IFINERRORS IFINOCTETS IFOUTERRORS IFOUTOCTETS );
+        }
+    }
+
+    # Update components if necessary
+    if ($device->{COMPONENTS} && ref($device->{COMPONENTS}->{COMPONENT}) eq 'ARRAY') {
+        my $components = $device->{COMPONENTS}->{COMPONENT};
+
+        my $model = $self->walk(jnxVirtualChassisMemberModel);
+        my $serial = $self->walk(jnxVirtualChassisMemberSerialnumber);
+
+        # List chassis, container and module components
+        my @chassis   = grep { $_->{TYPE} && $_->{TYPE} eq 'chassis'   } @{$components};
+        my @container = grep { $_->{TYPE} && $_->{TYPE} eq 'container' } @{$components};
+
+        if (@container > 1) {
+            # First rename "chassis" as "virtualchassis" when containers also exists
+            map { $_->{TYPE} = 'virtualchassis' } @chassis;
+
+            # Then copy parent module container values
+            foreach my $container (@container) {
+                next unless $container->{SERIAL};
+                my $key = first { $serial->{$_} && getCanonicalString($serial->{$_}) eq $container->{SERIAL} } keys(%{$serial});
+                $container->{MODEL} = getCanonicalString($model->{$key}) if defined($key) && $model->{$key};
+                # Finally change container type to chassis
+                $container->{TYPE} = "chassis";
+            }
         }
     }
 }
