4cec968ccff4cb8fe4cef0d047a758f1d0677f0c
Guillaume Bougard <gbougard@teclib.com>
2026-03-04 13:06:25 +0100

    fix: Updated Riello UPS devices support Closes #1119

--- lib/GLPI/Agent/SNMP/MibSupport/UPS.pm
+++ lib/GLPI/Agent/SNMP/MibSupport/UPS.pm
@@ -9,7 +9,16 @@ use GLPI::Agent::Tools;
 use GLPI::Agent::Tools::SNMP;
 
 use constant    apc => '.1.3.6.1.4.1.318' ;
-use constant    riello => '.1.3.6.1.4.1.5491' ;
+
+# See RIELLO-MIB
+
+use constant    riello => '.1.3.6.1.4.1.5491';
+
+# See RIELLOUPS-MIB
+
+use constant    rupsIdentManufacturer       => riello . '.10.1.1.1.0';
+use constant    rupsIdentModel              => riello . '.10.1.1.2.0';
+use constant    rupsIdentUPSSoftwareVersion => riello . '.10.1.1.3.0';
 
 # See PowerNet-MIB
 
@@ -25,19 +34,32 @@ use constant    upsIdentManufacturer        => upsMIB  .'.1.1.1.0' ;
 use constant    upsIdentModel               => upsMIB  .'.1.1.2.0' ;
 use constant    upsIdentUPSSoftwareVersion  => upsMIB  .'.1.1.3.0' ;
 
-my $match = apc.'|'.upsMIB.'|'.riello;
+my $match = apc.'|'.upsMIB;
 
 our $mibSupport = [
     {
         name        => "apc",
-        sysobjectid => qr/^$match/
+        sysobjectid => getRegexpOidMatch(apc)
+    },
+    {
+        name        => "ups-mib",
+        sysobjectid => getRegexpOidMatch(upsMIB)
+    },
+    {
+        name        => "riello",
+        sysobjectid => getRegexpOidMatch(riello)
     }
 ];
 
 sub getModel {
     my ($self) = @_;
 
-    return $self->get(upsIdentModel) || $self->get(sPDUIdentModelNumber);
+    if ($self->is("riello")) {
+        my $model = getCanonicalString($self->get(rupsIdentModel));
+        return $model unless empty($model);
+    }
+
+    return getCanonicalString($self->get(upsIdentModel) || $self->get(sPDUIdentModelNumber));
 }
 
 sub getSerial {
@@ -49,11 +71,21 @@ sub getSerial {
 sub getFirmware {
     my ($self) = @_;
 
-    return $self->get(upsIdentUPSSoftwareVersion) || $self->get(sPDUIdentFirmwareRev);
+    if ($self->is("riello")) {
+        my $firmware = getCanonicalString($self->get(rupsIdentUPSSoftwareVersion));
+        return $firmware unless empty($firmware);
+    }
+
+    return getCanonicalString($self->get(upsIdentUPSSoftwareVersion) || $self->get(sPDUIdentFirmwareRev));
 }
 
 sub getManufacturer {
     my ($self) = @_;
+
+    if ($self->is("riello")) {
+        my $manufacturer = getCanonicalString($self->get(rupsIdentManufacturer));
+        return $manufacturer unless empty($manufacturer);
+    }
 
     return getCanonicalString($self->get(upsIdentManufacturer));
 }
