ef82628db773d7e7eb4702426f40627c7d1818a0
Guillaume Bougard <gbougard@teclib.com>
2026-03-23 08:50:12 +0100

    build: Use msys2-base 20260322 for windows packaging

--- contrib/windows/packaging/ToolchainBuildJob.pm
+++ contrib/windows/packaging/ToolchainBuildJob.pm
@@ -39,8 +39,8 @@ sub toolchain_build_steps {
             {
                 plugin          => 'Perl::Dist::Strawberry::Step::Msys2',
                 name            => 'msys2-base',
-                version         => '20251213',
-                folder          => '2025-12-13',
+                version         => '20260322',
+                folder          => '2026-03-22',
                 url             => 'https://github.com/msys2/msys2-installer/releases/download/<folder>/<name>-x86_64-<version>.tar.xz',
                 dest            => 'msys64',
             },
