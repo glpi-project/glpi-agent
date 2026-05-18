e9c47c5442be4954ebe53859177306487c11b4bf
Guillaume Bougard <gbougard@teclib.com>
2026-05-18 09:50:06 +0200

    build: Use gcc 16.1.0posix-14.0.0-msvcrt-r2 for windows build

--- contrib/windows/packaging/ToolchainBuildJob.pm
+++ contrib/windows/packaging/ToolchainBuildJob.pm
@@ -6,8 +6,8 @@ use parent 'Exporter';
 use constant {
     # Toolchain setup
     TOOLCHAIN_BASE_URL  => 'https://github.com/brechtsanders/winlibs_mingw/releases/download',
-    TOOLCHAIN_VERSION   => '16.1.0posix-14.0.0-msvcrt-r1',
-    TOOLCHAIN_ARCHIVE   => 'winlibs-x86_64-posix-seh-gcc-16.1.0-mingw-w64msvcrt-14.0.0-r1.zip'
+    TOOLCHAIN_VERSION   => '16.1.0posix-14.0.0-msvcrt-r2',
+    TOOLCHAIN_ARCHIVE   => 'winlibs-x86_64-posix-seh-gcc-16.1.0-mingw-w64msvcrt-14.0.0-r2.zip'
 };
 
 sub toolchain_build_steps {
