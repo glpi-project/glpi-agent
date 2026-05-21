d65cf13aa2eb5a84643c5980048de7ba64ab91f6
Gabriel MATIAS <gabriel.matias@easymile.com>
2026-05-21 12:23:47 +0200

    fix(packaging): fix windows build failure and cleanup PerlBuildJob.pm
    - Removed duplicated _movedll calls in Step 11 which caused 'movefile' to fail.
    - Cleaned up redundant module installation in Step 5.
    - Kept Crypt::Ed25519 for signature verification support.

--- contrib/windows/packaging/PerlBuildJob.pm
+++ contrib/windows/packaging/PerlBuildJob.pm
@@ -126,7 +126,8 @@ sub build_job {
                 qw/ HTTP-Server-Simple LWP::Protocol::https LWP::UserAgent /,
 
                 # crypto
-                qw/ Crypt::DES Crypt::Rijndael /,
+                { module => 'https://github.com/g-bougard/Crypt-DES/releases/download/2.07_01/Crypt-DES-2.07_01.tar.gz' }, # Patched Crypt::DES
+                qw/ Crypt::Rijndael /,
                 qw/ Digest-SHA /,
                 qw/ Digest-MD5 Digest-SHA1 Digest::HMAC /, # Required for SNMP v3 authentication
                 qw/ Crypt::Ed25519 /,
@@ -237,8 +238,6 @@ sub build_job {
                 # Also move DLLs required by modules
                 _movedll('libxml2-16', $dllsuffix),
                 _movedll('liblzma-5', $dllsuffix),
-                _movedll('libcharset-1', $dllsuffix),
-                _movedll('libiconv-2', $dllsuffix),
                 _movedll('libcharset-1', $dllsuffix),
                 _movedll('libiconv-2', $dllsuffix),
                 _movedll('libcrypto-3', $dllsuffix),
