cce255c625faf474d480d9059925afdd2aeaa530
eduardomozart <2974895+eduardomozart@users.noreply.github.com>
2026-06-08 18:16:41 -0300

    Improve Windows sound inventory naming and tests
    Enhance Win32 sound inventory: query Win32_PnPEntity to obtain better device names, prefer PnP names when available, and parse PNPDeviceID for PCI (VEN/DEV/SUBSYS) and USB (VID/PID) to resolve vendor and device names via getPCIDeviceVendor/getUSBDeviceVendor. Add GLPI::Agent::Tools::Generic import and escape PNP IDs when building WMI queries. Also add sample WMI resource files for a Logitech H390 and a new test (t/tasks/inventory/windows/sounds.t) to validate parsing and registration of sound entries.

--- lib/GLPI/Agent/Task/Inventory/Win32/Sounds.pm
+++ lib/GLPI/Agent/Task/Inventory/Win32/Sounds.pm
@@ -6,6 +6,7 @@ use warnings;
 use parent 'GLPI::Agent::Task::Inventory::Module';
 
 use GLPI::Agent::Tools::Win32;
+use GLPI::Agent::Tools::Generic;
 
 use constant    category    => "sound";
 
@@ -18,19 +19,76 @@ sub doInventory {
 
     my $inventory = $params{inventory};
 
-    foreach my $object (getWMIObjects(
+    my @sounds = getWMIObjects(
         class      => 'Win32_SoundDevice',
         properties => [ qw/
-            Name Manufacturer Caption Description
+            Name Manufacturer Caption Description PNPDeviceID
         / ]
-    )) {
+    );
+
+    # Try to find better names from Win32_PnPEntity if possible
+    my %pnp_names;
+    if (@sounds) {
+        my @pnp_ids = grep { defined $_ && length $_ } map { $_->{PNPDeviceID} } @sounds;
+        if (@pnp_ids) {
+            my $query = "SELECT DeviceID, Name FROM Win32_PnPEntity WHERE " .
+                        join(" OR ", map { my $id = $_; $id =~ s/\\/\\\\/g; "DeviceID='$id'" } @pnp_ids);
+
+            foreach my $pnp (getWMIObjects(query => $query, properties => [qw/DeviceID Name/])) {
+                $pnp_names{$pnp->{DeviceID}} = $pnp->{Name} if $pnp->{DeviceID};
+            }
+        }
+    }
+
+    foreach my $object (@sounds) {
+
+        my $pnp_name = $object->{PNPDeviceID} ? $pnp_names{$object->{PNPDeviceID}} : undef;
+
+        my $name         = $pnp_name || $object->{Name};
+        my $manufacturer = $object->{Manufacturer};
+        my $caption      = $object->{Caption};
+
+        if ($object->{PNPDeviceID}) {
+            if ($object->{PNPDeviceID} =~ /PCI\\VEN_(\S{4})&DEV_(\S{4})/) {
+                my $vendor_id = lc($1);
+                my $device_id = lc($2);
+                my $subdevice_id;
+
+                if ($object->{PNPDeviceID} =~ /&SUBSYS_(\S{4})(\S{4})/) {
+                    $subdevice_id = lc($2 . ':' . $1);
+                }
+
+                my $vendor = getPCIDeviceVendor(id => $vendor_id, %params);
+                if ($vendor) {
+                    $manufacturer = $vendor->{name} if $vendor->{name};
+                    if ($vendor->{devices}->{$device_id}) {
+                        my $entry = $vendor->{devices}->{$device_id};
+                        $name = $subdevice_id && $entry->{subdevices}->{$subdevice_id} ?
+                            $entry->{subdevices}->{$subdevice_id}->{name} :
+                            $entry->{name};
+                    }
+                }
+            } elsif ($object->{PNPDeviceID} =~ /USB\\VID_(\S{4})&PID_(\S{4})/) {
+                my $vendor_id = lc($1);
+                my $device_id = lc($2);
+
+                my $vendor = getUSBDeviceVendor(id => $vendor_id, %params);
+                if ($vendor) {
+                    $manufacturer = $vendor->{name} if $vendor->{name};
+                    if ($vendor->{devices}->{$device_id}) {
+                        my $entry = $vendor->{devices}->{$device_id};
+                        $name = $entry->{name} if $entry->{name};
+                    }
+                }
+            }
+        }
 
         $inventory->addEntry(
             section => 'SOUNDS',
             entry   => {
-                NAME         => $object->{Name},
-                CAPTION      => $object->{Caption},
-                MANUFACTURER => $object->{Manufacturer},
+                NAME         => $name,
+                CAPTION      => $caption,
+                MANUFACTURER => $manufacturer,
                 DESCRIPTION  => $object->{Description},
             }
         );
---
+++ t/tasks/inventory/windows/sounds.t
@@ -0,0 +1,72 @@
+#!/usr/bin/perl
+
+use strict;
+use warnings;
+use utf8;
+use lib 't/lib';
+
+use English qw(-no_match_vars);
+use Test::Deep;
+use Test::Exception;
+use Test::MockModule;
+use Test::More;
+use Test::NoWarnings;
+use UNIVERSAL::require;
+
+use GLPI::Agent::Inventory;
+use GLPI::Test::Utils;
+
+BEGIN {
+    # use mock modules for non-available ones
+    push @INC, 't/lib/fake/windows' if $OSNAME ne 'MSWin32';
+}
+
+use Config;
+# check thread support availability
+if (!$Config{usethreads} || $Config{usethreads} ne 'define') {
+    plan skip_all => 'thread support required';
+}
+
+GLPI::Agent::Task::Inventory::Win32::Sounds->require();
+
+my %tests = (
+    'logitech-h390' => [
+        {
+            CAPTION      => 'USB Audio Device',
+            DESCRIPTION  => 'USB Audio Device',
+            MANUFACTURER => 'Logitech, Inc.',
+            NAME         => 'H390 headset with microphone',
+        }
+    ],
+);
+
+plan tests => (2 * scalar keys %tests) + 1;
+
+my $inventory = GLPI::Agent::Inventory->new();
+
+my $module = Test::MockModule->new(
+    'GLPI::Agent::Task::Inventory::Win32::Sounds'
+);
+
+foreach my $test (keys %tests) {
+    $module->mock(
+        'getWMIObjects',
+        mockGetWMIObjects($test)
+    );
+
+    my $inventory_mock = GLPI::Agent::Inventory->new();
+    GLPI::Agent::Task::Inventory::Win32::Sounds::doInventory(
+        inventory => $inventory_mock,
+        datadir   => './share',
+    );
+
+    cmp_deeply(
+        $inventory_mock->getSection('SOUNDS'),
+        $tests{$test},
+        "$test: parsing"
+    );
+    lives_ok {
+        $inventory->addEntry(section => 'SOUNDS', entry => $_)
+            foreach @{$inventory_mock->getSection('SOUNDS') // []};
+    } "$test: registering";
+}
