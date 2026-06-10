c498088b2badaace314291d0cd4e3aa2a6abf560
eduardomozart <2974895+eduardomozart@users.noreply.github.com>
2026-06-10 16:56:06 -0300

    Hello world for Fix USB internal

--- lib/GLPI/Agent/Task/Inventory/Win32/Networks.pm
+++ lib/GLPI/Agent/Task/Inventory/Win32/Networks.pm
@@ -9,6 +9,10 @@ use GLPI::Agent::Tools;
 use GLPI::Agent::Tools::Network;
 use GLPI::Agent::Tools::Win32;
 
+our @runAfter = (
+    'GLPI::Agent::Task::Inventory::Win32::USB'
+);
+
 use constant    category    => "network";
 
 sub isEnabled {
@@ -42,7 +46,15 @@ sub doInventory {
         delete $interface->{DNSDomain};
         delete $interface->{GUID};
 
-        if ($interface->{PNPDEVICEID} && !$interface->{TYPE}) {
+        if ($interface->{PNPDEVICEID} && $interface->{PNPDEVICEID} =~ /^BTH/i) {
+            $interface->{TYPE} = 'bluetooth';
+
+            my $parentInfo = _getBluetoothParentInfo($interface->{PNPDEVICEID}, $inventory);
+            if ($parentInfo) {
+                $interface->{MANUFACTURER} = $parentInfo->{MANUFACTURER} if $parentInfo->{MANUFACTURER};
+                $interface->{MODEL}        = $parentInfo->{MODEL}        if $parentInfo->{MODEL};
+            }
+        } elsif ($interface->{PNPDEVICEID} && !$interface->{TYPE}) {
             my $type = _getMediaType($interface->{PNPDEVICEID}, $keys);
             $interface->{TYPE} = $type if defined($type);
         }
@@ -88,6 +100,111 @@ sub _getMediaType {
             $subtype eq '0x00000002' ? 'wifi'      :
             $subtype eq '0x00000007' ? 'bluetooth' :
                                        undef;
+}
+
+sub _getBluetoothParentInfo {
+    my ($deviceid, $inventory) = @_;
+
+    my $info;
+
+    UNIVERSAL::require('Win32::API');
+    return $info unless $Win32::API::VERSION;
+
+    require Encode;
+
+    my $CM_Locate_DevNodeW = Win32::API->new('cfgmgr32.dll', 'CM_Locate_DevNodeW', 'PPI', 'I');
+    my $CM_Get_Parent      = Win32::API->new('cfgmgr32.dll', 'CM_Get_Parent', 'PII', 'I');
+    my $CM_Get_Device_IDW  = Win32::API->new('cfgmgr32.dll', 'CM_Get_Device_IDW', 'IPII', 'I');
+
+    return $info unless $CM_Locate_DevNodeW && $CM_Get_Parent && $CM_Get_Device_IDW;
+
+    my $deviceIdW = Encode::encode('UTF-16LE', $deviceid . "\0");
+    my $devInst = pack('L', 0);
+
+    if ($CM_Locate_DevNodeW->Call($devInst, $deviceIdW, 0) == 0) {
+        my $dnDevInst = unpack('L', $devInst);
+        my $parentInst = pack('L', 0);
+        if ($CM_Get_Parent->Call($parentInst, $dnDevInst, 0) == 0) {
+            my $dnParentInst = unpack('L', $parentInst);
+            my $buffer = "\0" x 512;
+            if ($CM_Get_Device_IDW->Call($dnParentInst, $buffer, 256, 0) == 0) {
+                my $parentDeviceId = Encode::decode('UTF-16LE', $buffer);
+                $parentDeviceId =~ s/\0.*//;
+
+                my $matched_usb;
+                if ($inventory) {
+                    my $usbDevices = $inventory->getSection('USBDEVICES');
+                    if ($usbDevices) {
+                        if (my ($vid, $pid, $serial) = $parentDeviceId =~ /^USB\\VID_([0-9A-F]+)&PID_([0-9A-F]+)\\(.*)/i) {
+                            for (my $i = 0; $i < @$usbDevices; $i++) {
+                                my $dev = $usbDevices->[$i];
+                                my $dvid = $dev->{VENDORID} || '';
+                                my $dpid = $dev->{PRODUCTID} || '';
+                                my $dser = $dev->{SERIAL} || '';
+                                
+                                # Support the manufacturer pseudo-serial workaround present in USB.pm
+                                my $clean_serial = $serial;
+                                $clean_serial = $1 if $clean_serial =~ /^S\/N:([0-9A-F]+)/i;
+                                
+                                if (lc($dvid) eq lc($vid) && lc($dpid) eq lc($pid) && (lc($dser) eq lc($clean_serial) || $dser eq '')) {
+                                    $matched_usb = splice(@$usbDevices, $i, 1);
+                                    last;
+                                }
+                            }
+                        }
+                    }
+                }
+
+                if ($matched_usb) {
+                    $info->{MANUFACTURER} = $matched_usb->{MANUFACTURER} if $matched_usb->{MANUFACTURER};
+                    $info->{MODEL}        = $matched_usb->{NAME} || $matched_usb->{CAPTION};
+                } else {
+                    if ($parentDeviceId =~ /^USB\\VID_([0-9A-F]+)&PID_([0-9A-F]+)/i) {
+                        my $vid = $1;
+                        my $pid = $2;
+                        UNIVERSAL::require('GLPI::Agent::Tools::Generic');
+                        if ($GLPI::Agent::Tools::Generic::VERSION || defined(&GLPI::Agent::Tools::Generic::getUSBDeviceVendor)) {
+                            my $vendor = GLPI::Agent::Tools::Generic::getUSBDeviceVendor(id => lc($vid));
+                            if ($vendor) {
+                                $info->{MANUFACTURER} = $vendor->{name} if $vendor->{name};
+                                my $device = $vendor->{devices}->{lc($pid)};
+                                $info->{MODEL} = $device->{name} if $device && $device->{name};
+                            }
+                        }
+                    } elsif ($parentDeviceId =~ /^PCI\\VEN_([0-9A-F]+)&DEV_([0-9A-F]+)/i) {
+                        my $ven = $1;
+                        my $dev = $2;
+                        UNIVERSAL::require('GLPI::Agent::Tools::Generic');
+                        if ($GLPI::Agent::Tools::Generic::VERSION || defined(&GLPI::Agent::Tools::Generic::getPCIDeviceVendor)) {
+                            my $vendor = GLPI::Agent::Tools::Generic::getPCIDeviceVendor(id => lc($ven));
+                            if ($vendor) {
+                                $info->{MANUFACTURER} = $vendor->{name} if $vendor->{name};
+                                my $device = $vendor->{devices}->{lc($dev)};
+                                $info->{MODEL} = $device->{name} if $device && $device->{name};
+                            }
+                        }
+                    }
+
+                    if (!$info->{MANUFACTURER} || !$info->{MODEL}) {
+                        my $wmiQueryId = $parentDeviceId;
+                        $wmiQueryId =~ s/\\/\\\\/g;
+                        my ($parentDev) = GLPI::Agent::Tools::Win32::getWMIObjects(
+                            class      => 'Win32_PnPEntity',
+                            properties => [ qw/Manufacturer Caption/ ],
+                            query      => "SELECT Manufacturer, Caption FROM Win32_PnPEntity WHERE PNPDeviceID='$wmiQueryId'"
+                        );
+
+                        if ($parentDev) {
+                            $info->{MANUFACTURER} = $parentDev->{Manufacturer} if !$info->{MANUFACTURER} && $parentDev->{Manufacturer};
+                            $info->{MODEL}        = $parentDev->{Caption}      if !$info->{MODEL}        && $parentDev->{Caption};
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    return $info;
 }
 
 1;
--- t/tasks/inventory/windows/networks.t
+++ t/tasks/inventory/windows/networks.t
@@ -6,6 +6,7 @@ use lib 't/lib';
 
 use English qw(-no_match_vars);
 use Test::More;
+use Test::MockModule;
 use UNIVERSAL::require;
 
 use GLPI::Test::Utils;
@@ -36,7 +37,7 @@ my %tests = (
     },
 );
 
-my $plan = 1;
+my $plan = 3; # Base 1 + 2 tests for bluetooth info
 foreach my $test (keys %tests) {
     $plan += scalar (keys %{$tests{$test}});
 }
@@ -47,6 +48,40 @@ foreach my $test (keys %tests) {
     my $file = "resources/win32/registry/$test-{4D36E972-E325-11CE-BFC1-08002BE10318}.reg";
     my $keys = loadRegistryDump($file);
 
+    my $api_module = Test::MockModule->new('Win32::API');
+    $api_module->mock('new', sub {
+        my ($class, $dll, $func, $in, $out) = @_;
+        my $self = { func => $func };
+        return bless $self, 'Mock::Win32::API';
+    });
+
+    # Define the Mock package for Call
+    {
+        no warnings 'redefine';
+        package Mock::Win32::API;
+        sub Call {
+            my ($self, @args) = @_;
+            if ($self->{func} eq 'CM_Locate_DevNodeW') {
+                $_[1] = pack('L', 1234); # Fake devInst
+                return 0;
+            }
+            if ($self->{func} eq 'CM_Get_Parent') {
+                $_[1] = pack('L', 5678); # Fake parentInst
+                return 0;
+            }
+            if ($self->{func} eq 'CM_Get_Device_IDW') {
+                require Encode;
+                my $encoded = Encode::encode('UTF-16LE', "USB\\VID_0BDA&PID_C829\\00E04C000001\0");
+                substr($_[2], 0, length($encoded)) = $encoded;
+                return 0;
+            }
+            return 1;
+        }
+    }
+
+    my $net_module = Test::MockModule->new('GLPI::Agent::Tools::Win32');
+    $net_module->mock('getWMIObjects', mockGetWMIObjects($test));
+
     foreach my $deviceId (keys %{$tests{$test}}) {
         is(
             GLPI::Agent::Task::Inventory::Win32::Networks::_getMediaType($deviceId, $keys),
@@ -54,4 +89,9 @@ foreach my $test (keys %tests) {
             "$test sample, $deviceId device"
         );
     }
+
+    my $bt_info = GLPI::Agent::Task::Inventory::Win32::Networks::_getBluetoothParentInfo('BTH\MS_BTHPAN\6&1AAC2CAC&0&2');
+    is($bt_info->{MANUFACTURER}, 'Realtek Semiconductor Corp.', "$test sample, Bluetooth parent manufacturer");
+    is($bt_info->{MODEL}, 'Realtek Bluetooth Adapter', "$test sample, Bluetooth parent model");
 }
+
