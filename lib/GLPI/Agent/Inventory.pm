3c152c155a17fdbe5ccc7170de2b6c9ffa58de61
Guillaume Bougard <gbougard@teclib.com>
2026-07-22 10:55:27 +0200

    Updated inventory format checks (#1168)
    * fix: Updated inventory format checks
    Support checking format by ref

--- lib/GLPI/Agent/Inventory.pm
+++ lib/GLPI/Agent/Inventory.pm
@@ -120,6 +120,12 @@ my %checks = (
         }
     },
     VIRTUALMACHINES => {
+        IPADDRESS => {
+            not_before  => glpiVersion('10.0.17'),
+        },
+        OPERATINGSYSTEM => {
+            not_before  => glpiVersion('10.0.17'),
+        },
         STATUS => qr/^(running|blocked|idle|paused|shutdown|crashed|dying|off)$/
     },
     SLOTS => {
@@ -410,6 +416,37 @@ sub addEntry {
                 $self->{logger}->debug(
                     "invalid value $value for field $field for section $section"
                 ) unless $value =~ $checks->{$field}->{regexp};
+            } elsif ($checks->{$field}->{not_before} && $checks->{$field}->{not_before} > $self->{_glpi_version}) {
+                $self->{logger}->debug(
+                    "unsupported field $field for section $section"
+                );
+                delete $entry->{$field};
+                next;
+            }
+            if ($checks->{$field}->{as_ref} && ref($entry->{$field})) {
+                my $ref = $checks->{$field}->{as_ref};
+                if (ref($entry->{$field}) eq 'HASH') {
+                    $value = $self->_checkEntry($entry->{$field}, $ref);
+                    unless (defined($value)) {
+                        $self->{logger}->debug(
+                            "unsupported hash ref field $field for section $section"
+                        );
+                        next;
+                    }
+                } elsif(ref($entry->{$field}) eq 'ARRAY') {
+                    $value = [];
+                    foreach my $fieldentry (@{$entry->{$field}}) {
+                        next unless ref($fieldentry) eq 'HASH';
+                        my $val = $self->_checkEntry($fieldentry, $ref);
+                        push @{$value}, $val if defined($val);
+                    }
+                    unless (scalar(@{$value})) {
+                        $self->{logger}->debug(
+                            "unsupported array ref field $field for section $section"
+                        );
+                        next;
+                    }
+                }
             }
         } elsif ($checks->{$field}) {
             $self->{logger}->debug(
@@ -424,6 +461,55 @@ sub addEntry {
     }
 
     push @{$self->{content}{$section}}, $entry;
+}
+
+sub _checkEntry {
+    my ($self, $entry, $section) = @_;
+
+    my $fields = $fields{$section};
+    unless ($fields) {
+        $self->{logger}->debug("No field support for $section insertion");
+        return;
+    }
+
+    my $checks = $checks{$section};
+
+    foreach my $field (keys %$entry) {
+        if (!$fields->{$field}) {
+            # unvalid field, log error and remove
+            $self->{logger}->debug("unknown field $field for section $section");
+            delete $entry->{$field};
+            next;
+        }
+        if (!defined $entry->{$field}) {
+            # undefined value, remove
+            delete $entry->{$field};
+            next;
+        }
+        # sanitize value
+        my $value = getSanitizedString($entry->{$field});
+        # check value if appliable
+        if (ref($checks->{$field}) eq 'HASH') {
+            if ($checks->{$field}->{regexp} && $checks->{$field}->{not_since} && $checks->{$field}->{not_since} > $self->{_glpi_version}) {
+                $self->{logger}->debug(
+                    "invalid value $value for field $field for section $section"
+                ) unless $value =~ $checks->{$field}->{regexp};
+            } elsif ($checks->{$field}->{not_before} && $checks->{$field}->{not_before} > $self->{_glpi_version}) {
+                $self->{logger}->debug(
+                    "still unsupported field $field for section $section"
+                );
+                delete $entry->{$field};
+                next;
+            }
+        } elsif ($checks->{$field}) {
+            $self->{logger}->debug(
+                "invalid value $value for field $field for section $section"
+            ) unless $value =~ $checks->{$field};
+        }
+        $entry->{$field} = $value;
+    }
+
+    return $entry;
 }
 
 sub setEntry {
