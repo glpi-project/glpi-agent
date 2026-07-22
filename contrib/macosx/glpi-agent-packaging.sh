7c30a3f4f3e9775c1d47daa0104c13b29633db85
Guillaume Bougard <gbougard@teclib.com>
2026-07-22 16:04:06 +0200

    build: Use perl 5.44.0 for windows, MacOSX and AppImage packaging

--- contrib/macosx/glpi-agent-packaging.sh
+++ contrib/macosx/glpi-agent-packaging.sh
@@ -3,7 +3,7 @@
 # PERL: https://www.perl.org/get.html
 # SSL:  https://github.com/openssl/openssl/releases
 # ZLIB: https://www.zlib.net/
-: ${PERL_VERSION:=5.42.2}
+: ${PERL_VERSION:=5.44.0}
 : ${OPENSSL_VERSION:=3.5.7}
 : ${ZLIB_VERSION:=1.3.2}
 : ${ZLIB_SHA256:=bb329a0a2cd0274d05519d61c667c062e06990d72e125ee2dfa8de64f0119d16}
