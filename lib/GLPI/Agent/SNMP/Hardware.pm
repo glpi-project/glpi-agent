74539e55a2a5f29151de4b52286a3959fdf1d183
Guillaume Bougard <gbougard@teclib.com>
2026-07-27 15:53:53 +0200

    fix: Fix test in previous commit

--- lib/GLPI/Agent/SNMP/Hardware.pm
+++ lib/GLPI/Agent/SNMP/Hardware.pm
@@ -1133,7 +1133,7 @@ sub _setConnectedDevices {
                 my $match = 0;
 
                 # Try different case to find LLDP/CDP connection match
-                if ($lldp_connection->{SYSDESCR} && !empty($cdp_connection) && $cdp_connection->{SYSDESCR} eq $lldp_connection->{SYSDESCR}) {
+                if ($lldp_connection->{SYSDESCR} && !empty($cdp_connection->{SYSDESCR}) && $cdp_connection->{SYSDESCR} eq $lldp_connection->{SYSDESCR}) {
                     $match ++;
                 } elsif ($lldp_connection->{SYSNAME} && $cdp_connection->{SYSNAME}) {
                     my $cdp_test = getCanonicalMacAddress($cdp_connection->{SYSNAME});
