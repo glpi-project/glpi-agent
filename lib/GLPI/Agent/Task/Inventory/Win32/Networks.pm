3711a27a6268e9e80f4007605f775a515a9aa6c4
Eduardo Mozart de Oliveira <2974895+eduardomozart@users.noreply.github.com>
2026-07-31 10:55:57 -0300

    feat(win32/networks): native parent resolution and USB/PCI ID correlation for Bluetooth adapters (#1188)
    Co-authored-by: Guillaume Bougard <gbougard@teclib.com>

--- lib/GLPI/Agent/Task/Inventory/Win32/Networks.pm
+++ lib/GLPI/Agent/Task/Inventory/Win32/Networks.pm
@@ -8,6 +8,7 @@ use parent 'GLPI::Agent::Task::Inventory::Module';
 use GLPI::Agent::Tools;
 use GLPI::Agent::Tools::Network;
 use GLPI::Agent::Tools::Win32;
+use GLPI::Agent::Tools::Win32::Bluetooth;
 
 use constant    category    => "network";
 
@@ -41,6 +42,14 @@ sub doInventory {
         delete $interface->{dns};
         delete $interface->{DNSDomain};
         delete $interface->{GUID};
+
+        if ($interface->{PNPDEVICEID} && $interface->{PNPDEVICEID} =~ /^BTH/i) {
+            my $parentInfo = getBluetoothParentInfo($interface->{PNPDEVICEID});
+            if ($parentInfo) {
+                $interface->{MANUFACTURER} = $parentInfo->{MANUFACTURER} if $parentInfo->{MANUFACTURER};
+                $interface->{MODEL}        = $parentInfo->{MODEL}        if $parentInfo->{MODEL};
+            }
+        }
 
         if ($interface->{PNPDEVICEID} && !$interface->{TYPE}) {
             my $type = _getMediaType($interface->{PNPDEVICEID}, $keys);
