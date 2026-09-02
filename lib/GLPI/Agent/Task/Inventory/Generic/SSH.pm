b98c36ee81053022b959c7090e41b7a528efa16c
Guillaume Bougard <gbougard@teclib.com>
2026-09-02 18:18:25 +0200

    fix: Fix localhost ssh pubkey inventory to limit requests Closes #1208

--- lib/GLPI/Agent/Task/Inventory/Generic/SSH.pm
+++ lib/GLPI/Agent/Task/Inventory/Generic/SSH.pm
@@ -19,22 +19,41 @@ sub doInventory {
     my $inventory = $params{inventory};
 
     my $port;
-    my $command = "ssh-keyscan";
+    my $command = [ "ssh-keyscan" ];
     if (canRead('/etc/ssh/sshd_config')) {
         foreach my $line (getAllLines( file => '/etc/ssh/sshd_config' )) {
             next unless $line =~ /^Port\s+(\d+)/;
             $port = $1;
         }
     }
-    $command .= " -p $port" if $port;
+    push @{$command}, "-p", $port if $port;
 
-    # Use a 1 second timeout instead of default 5 seconds as this is still
-    # large enough for loopback ssh pubkey scan.
-    $command .= ' -T 1 127.0.0.1';
-    my @ssh_keys = sort map { /^\S+\s(ssh.*)/ && $1 } grep { /^[^#]\S+\sssh/ } getAllLines(
-        command => $command,
-        @_,
+    my @ssh_keys;
+
+    # Get signing algorithm supported list
+    my @sig_algo = getAllLines(
+        command => [ qw(ssh -Q sig) ],
+        @_
     );
+
+    # Get first found ssh pub key with supported signing algorithms
+    foreach my $algo (@sig_algo) {
+        @ssh_keys = sort map { /^\S+\s(ssh.*)/ && $1 } grep { /^[^#]\S+\sssh/ } getAllLines(
+            command => [ @{$command}, "-t", $algo, "-T", 1, "127.0.0.1" ],
+            @_,
+        ) and last;
+    }
+
+    # Fallback on old algorithm in the case new one doesn't find a key
+    unless (@ssh_keys) {
+        # Use a 1 second timeout instead of default 5 seconds as this is still
+        # large enough for loopback ssh pubkey scan.
+        push @{$command}, "-T", 1, "127.0.0.1";
+        @ssh_keys = sort map { /^\S+\s(ssh.*)/ && $1 } grep { /^[^#]\S+\sssh/ } getAllLines(
+            command => $command,
+            @_,
+        );
+    }
 
     $inventory->setOperatingSystem({
         SSH_KEY => $ssh_keys[0]
